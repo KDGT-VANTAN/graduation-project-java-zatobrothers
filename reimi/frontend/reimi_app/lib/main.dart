67f705aa638369edce0ddb9de9ebff0f620aff81
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-11-30 01:53:10 +0900

    feat: デバイスのロケール設定に基づいて初期言語を設定

--- reimi/frontend/reimi_app/lib/main.dart
+++ reimi/frontend/reimi_app/lib/main.dart
@@ -3,16 +3,20 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:reimi_app/data/datasources/firebase/firebase_options_dev.dart'
     as dev;
+import 'package:reimi_app/i18n/strings.g.dart';
 import 'package:reimi_app/my_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  LocaleSettings.useDeviceLocale(); // デバイスのロケール設定に基づいて初期言語を設定
   await Firebase.initializeApp(
     options: dev.DefaultFirebaseOptions.currentPlatform,
   );
   runApp(
-    const ProviderScope(
-      child: MyApp(),
+    ProviderScope(
+      child: TranslationProvider(
+        child: const MyApp(),
+      ),
     ),
   );
 }
