a79b9485b086dfce2869528fef28c57180204c67
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-12-01 15:05:25 +0900

    feat: サインイン・サインアップ画面で使用するテキストを追加

--- reimi/frontend/reimi_app/lib/i18n/strings.g.dart
+++ reimi/frontend/reimi_app/lib/i18n/strings.g.dart
@@ -4,9 +4,9 @@
 /// To regenerate, run: `dart run slang`
 ///
 /// Locales: 2
-/// Strings: 16 (8 per locale)
+/// Strings: 54 (27 per locale)
 ///
-/// Built on 2025-11-29 at 13:57 UTC
+/// Built on 2025-11-30 at 17:00 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
