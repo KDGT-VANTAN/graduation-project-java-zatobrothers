7abcc36a64631e3d1725b36ee8ea9e8f3e9c5c88
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-12-01 15:03:26 +0900

    feat: アプリアイコンを表示させるウィジェットを作成

---
+++ reimi/frontend/reimi_app/lib/shared/widgets/app_icon.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:reimi_app/gen/assets.gen.dart';
+
+class AppIcon extends StatelessWidget {
+  const AppIcon({
+    super.key,
+    required this.width,
+    required this.height,
+  });
+  final double? width;
+  final double? height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.3),
+            blurRadius: 20,
+          )
+        ],
+      ),
+      clipBehavior: Clip.antiAlias,
+      child: Assets.images.icons.appIcon.image(
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
