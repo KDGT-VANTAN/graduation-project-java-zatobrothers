64bda6ccce595f5925929ef6da834df5906bc218
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-12-10 13:15:38 +0900

    feat: ユーザー登録画面で共通化して使う部分のベースとなるウィジェットを作成

---
+++ reimi/frontend/reimi_app/lib/features/user_registration/presentation/widgets/user_registration_base_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:reimi_app/features/user_registration/application/user_registration_notifier.dart';
+import 'package:reimi_app/features/user_registration/presentation/widgets/next_button.dart';
+import 'package:reimi_app/features/user_registration/presentation/widgets/user_registration_header.dart';
+import 'package:reimi_app/i18n/strings.g.dart';
+import 'package:reimi_app/shared/widgets/arc_background.dart';
+
+class UserRegistrationBaseScreen extends ConsumerWidget {
+  const UserRegistrationBaseScreen({
+    super.key,
+    required this.question,
+    required this.theme,
+    required this.mainContent,
+    required this.answered,
+    required this.nextButtonOnPressed,
+  });
+  final String question;
+  final ThemeData theme;
+  final List<Widget> mainContent;
+  final bool answered;
+  final void Function()? nextButtonOnPressed;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final t = Translations.of(context).userRegistration.common;
+    final notifier = ref.read(userRegistrationProvider.notifier);
+    final currentPage = ref.watch(
+      userRegistrationProvider.select((state) => state.currentPage),
+    );
+    final totalPages = ref.watch(
+      userRegistrationProvider.select((state) => state.totalPages),
+    );
+    return Scaffold(
+      backgroundColor: theme.colorScheme.primary,
+      body: Stack(
+        children: [
+          ...ArcBackground.buildArcs(
+            context: context,
+            topArcColor: theme.colorScheme.secondary,
+            bottomArcColor: theme.colorScheme.tertiary,
+            topArcWidth: 40,
+            bottomArcWidth: 40,
+            topOffset: 0.25,
+            rightOffset: 0.4,
+            leftOffset: 0.35,
+            bottomOffset: 0.3,
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 8),
+                  UserRegistrationHeader(
+                    currentPage: currentPage,
+                    totalPages: totalPages,
+                    onPressed: () {
+                      notifier.previousPage();
+                      context.pop();
+                    },
+                  ),
+                  const SizedBox(height: 40),
+                  Text(
+                    question,
+                    style: theme.textTheme.headlineMedium!.copyWith(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  ...mainContent,
+                  const Spacer(),
+                  NextButton(
+                    label: t.button.next,
+                    theme: theme,
+                    answered: answered,
+                    onPressed: nextButtonOnPressed,
+                  ),
+                  const SizedBox(height: 16),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
