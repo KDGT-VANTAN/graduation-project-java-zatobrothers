e456471bd89a5131d1cd5c82ab7752a3f4663332
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-11 01:23:19 +0900

    feat: OpenAPIドキュメントに関するメタ情報を定義

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/ReimiAppApplication.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/ReimiAppApplication.java
@@ -3,6 +3,24 @@ package com.reimi.reimi_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Reimi",
+		version = "1.0",
+		description = "天気で繋がる、出会いのアプリ"
+	),
+	servers = {
+        @Server(
+            description = "ローカル環境",
+            url = "http://localhost:8080"
+		)
+	}
+)
+
 @SpringBootApplication
 public class ReimiAppApplication {
 
