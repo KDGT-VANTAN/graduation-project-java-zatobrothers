cf153b8591d04938da1dfa1e9286229cbd6c62cc
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-12-10 13:45:09 +0900

    faet: ユーザー登録時の各画面を追加

--- reimi/frontend/reimi_app/lib/routers/app_router.dart
+++ reimi/frontend/reimi_app/lib/routers/app_router.dart
@@ -8,6 +8,12 @@ import 'package:reimi_app/features/like/presentation/like_screen.dart';
 import 'package:reimi_app/features/matching/presentation/home_screen.dart';
 import 'package:reimi_app/features/profile/presentation/account_screen.dart';
 import 'package:reimi_app/features/splash/presentation/splash_screen.dart';
+import 'package:reimi_app/features/user_registration/presentation/screens/user_address_screen.dart';
+import 'package:reimi_app/features/user_registration/presentation/screens/user_birthdate_screen.dart';
+import 'package:reimi_app/features/user_registration/presentation/screens/user_gender_screen.dart';
+import 'package:reimi_app/features/user_registration/presentation/screens/user_introduction_screen.dart';
+import 'package:reimi_app/features/user_registration/presentation/screens/user_main_photo_screen.dart';
+import 'package:reimi_app/features/user_registration/presentation/screens/user_name_screen.dart';
 import 'package:reimi_app/features/weather_report/presentation/weather_report_screen.dart';
 import 'package:reimi_app/shared/widgets/bottom_navigation.dart';
 
@@ -20,6 +26,7 @@ final goRouterProvider = Provider<GoRouter>(
       debugLogDiagnostics: true,
       initialLocation: SplashScreen.routeLocation,
       routes: [
+        // ----- splash -----
         GoRoute(
           path: SplashScreen.routeLocation,
           name: SplashScreen.routeName,
@@ -27,6 +34,7 @@ final goRouterProvider = Provider<GoRouter>(
             return const SplashScreen();
           },
         ),
+        // ----- auth -----
         GoRoute(
           path: SignUpScreen.routeLocation,
           name: SignUpScreen.routeName,
@@ -41,6 +49,50 @@ final goRouterProvider = Provider<GoRouter>(
             return const SignInScreen();
           },
         ),
+        // ----- user_registration -----
+        GoRoute(
+          path: UserGenderScreen.routeLocation,
+          name: UserGenderScreen.routeName,
+          builder: (context, state) {
+            return const UserGenderScreen();
+          },
+        ),
+        GoRoute(
+          path: UserBirthdateScreen.routeLocation,
+          name: UserBirthdateScreen.routeName,
+          builder: (context, state) {
+            return const UserBirthdateScreen();
+          },
+        ),
+        GoRoute(
+          path: UserAddressScreen.routeLocation,
+          name: UserAddressScreen.routeName,
+          builder: (context, state) {
+            return const UserAddressScreen();
+          },
+        ),
+        GoRoute(
+          path: UserNameScreen.routeLocation,
+          name: UserNameScreen.routeName,
+          builder: (context, state) {
+            return const UserNameScreen();
+          },
+        ),
+        GoRoute(
+          path: UserIntroductionScreen.routeLocation,
+          name: UserIntroductionScreen.routeName,
+          builder: (context, state) {
+            return const UserIntroductionScreen();
+          },
+        ),
+        GoRoute(
+          path: UserMainPhotoScreen.routeLocation,
+          name: UserMainPhotoScreen.routeName,
+          builder: (context, state) {
+            return const UserMainPhotoScreen();
+          },
+        ),
+        // ----- bottom_navigation
         StatefulShellRoute.indexedStack(
           builder: (context, state, navigationShell) =>
               BottomNavigation(navigationShell: navigationShell),
@@ -48,36 +100,41 @@ final goRouterProvider = Provider<GoRouter>(
             StatefulShellBranch(
               routes: [
                 GoRoute(
-                    path: HomeScreen.routeLocation,
-                    builder: (context, state) => const HomeScreen()),
+                  path: HomeScreen.routeLocation,
+                  builder: (context, state) => const HomeScreen(),
+                ),
               ],
             ),
             StatefulShellBranch(
               routes: [
                 GoRoute(
-                    path: LikeScreen.routeLocation,
-                    builder: (context, state) => const LikeScreen()),
+                  path: LikeScreen.routeLocation,
+                  builder: (context, state) => const LikeScreen(),
+                ),
               ],
             ),
             StatefulShellBranch(
               routes: [
                 GoRoute(
-                    path: ChatScreen.routeLocation,
-                    builder: (context, state) => const ChatScreen()),
+                  path: ChatScreen.routeLocation,
+                  builder: (context, state) => const ChatScreen(),
+                ),
               ],
             ),
             StatefulShellBranch(
               routes: [
                 GoRoute(
-                    path: WeatherReportScreen.routeLocation,
-                    builder: (context, state) => const WeatherReportScreen()),
+                  path: WeatherReportScreen.routeLocation,
+                  builder: (context, state) => const WeatherReportScreen(),
+                ),
               ],
             ),
             StatefulShellBranch(
               routes: [
                 GoRoute(
-                    path: AccountScreen.routeLocation,
-                    builder: (context, state) => const AccountScreen()),
+                  path: AccountScreen.routeLocation,
+                  builder: (context, state) => const AccountScreen(),
+                ),
               ],
             ),
           ],
