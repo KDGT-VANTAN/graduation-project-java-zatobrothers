540967edf3ff12ee9cef249cc3b55013bb362bd6
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-12-10 13:46:42 +0900

    feat: ユーザー登録で使用するモデルを作成

---
+++ reimi/frontend/reimi_app/lib/features/user_registration/domain/entities/user_registration_model.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:reimi_app/domain/values/address.dart';
+import 'package:reimi_app/domain/values/gender.dart';
+
+part 'user_registration_model.freezed.dart';
+part 'user_registration_model.g.dart';
+
+@freezed
+abstract class UserRegistrationModel with _$UserRegistrationModel {
+  @JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+  const factory UserRegistrationModel({
+    Gender? gender,
+    DateTime? birthDate,
+    Address? address,
+    String? name,
+    String? introduction,
+    String? mainPhotoUrl,
+  }) = _UserRegistrationModel;
+
+  factory UserRegistrationModel.fromJson(Map<String, dynamic> json) => _$UserRegistrationModelFromJson(json);
+}
