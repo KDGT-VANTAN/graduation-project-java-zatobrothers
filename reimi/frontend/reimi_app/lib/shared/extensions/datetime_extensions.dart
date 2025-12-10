fd6e888d50ac4ab2296bd848e136e21b4a28aa37
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-12-10 13:46:08 +0900

    feat: 日付の表示形式を変更するextensionを追加

---
+++ reimi/frontend/reimi_app/lib/shared/extensions/datetime_extensions.dart
@@ -0,0 +1,19 @@
+import 'package:intl/intl.dart';
+
+extension DateTimeFormatExtension on DateTime {
+  // yyyy年MM月dd日形式
+  String get toJapaneseDate => DateFormat('yyyy年MM月dd日').format(this);
+
+  // yyyy年M月d日形式(0埋めなし)
+  String get toJapaneseDateShort => DateFormat('yyyy年M月d日').format(this);
+
+  // yyyy-MM-dd形式
+  String get toIsoDate => DateFormat('yyyy-MM-dd').format(this);
+
+  // yyyy/MM/dd形式
+  String get toSlashDate => DateFormat('yyyy/MM/dd').format(this);
+
+  // 曜日付き
+  String get toJapaneseDateWithWeekday =>
+      DateFormat('yyyy年MM月dd日(E)', 'ja').format(this);
+}
