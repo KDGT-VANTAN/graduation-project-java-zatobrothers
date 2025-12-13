3cae4cded8592a195e90d4761099ce88a2322730
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-12-14 02:33:53 +0900

    feat: ドットのローディングアニメーションを中央に表示させるウィジェットを作成(ローディング時に使用する予定)

---
+++ reimi/frontend/reimi_app/lib/presentation/shared/pages/blur_loading_overlay.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:reimi_app/presentation/pages/splash/components/loading_dots.dart';
+
+class BlurLoadingOverlay extends StatelessWidget {
+  const BlurLoadingOverlay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+          child: Container(
+            color: Colors.black.withValues(alpha: 0.1),
+          ),
+        ),
+        const Center(
+          child: LoadingDots(),
+        ),
+      ],
+    );
+  }
+}
