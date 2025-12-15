d443739ddd972dbe012bb899510ae314efff46a0
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-12-15 16:59:36 +0900

    refactor(core): Feature-FirstからState-Drivenなクリーンアーキテクチャへ全面移行

---
+++ reimi/frontend/reimi_app/lib/presentation/notifiers/feature/user_registration_notifier.dart
@@ -0,0 +1,61 @@
+import 'package:reimi_app/data/models/user_registration_model.dart';
+import 'package:reimi_app/domain/value_objects/address.dart';
+import 'package:reimi_app/domain/value_objects/gender.dart';
+import 'package:reimi_app/presentation/states/feature/user_registration_state.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'user_registration_notifier.g.dart';
+
+@riverpod
+class UserRegistrationNotifier extends _$UserRegistrationNotifier {
+  @override
+  UserRegistrationState build() {
+    return const UserRegistrationState(
+      data: UserRegistrationModel(),
+    );
+  }
+
+  void nextPage() {
+    if (state.currentPage < state.totalPages) {
+      state = state.copyWith(currentPage: state.currentPage + 1);
+    }
+  }
+
+  void previousPage() {
+    if (state.currentPage > 1) {
+      state = state.copyWith(currentPage: state.currentPage - 1);
+    }
+  }
+
+  void updateGender(Gender gender) {
+    state = state.copyWith(data: state.data!.copyWith(gender: gender));
+  }
+
+  void updateBirthDate(DateTime birthDate) {
+    state = state.copyWith(data: state.data!.copyWith(birthDate: birthDate));
+  }
+
+  void updateAddress(Address address) {
+    state = state.copyWith(data: state.data!.copyWith(address: address));
+  }
+
+  void updateName(String name) {
+    state = state.copyWith(data: state.data!.copyWith(name: name));
+  }
+
+  void updateIntroduction(String introduction) {
+    state = state.copyWith(
+      data: state.data!.copyWith(introduction: introduction),
+    );
+  }
+
+  void updateMainImage(String mainPhotoUrl) {
+    state = state.copyWith(
+      data: state.data!.copyWith(mainPhotoUrl: mainPhotoUrl),
+    );
+  }
+
+  Future<void> submit() async {
+    // 送信処理
+  }
+}
