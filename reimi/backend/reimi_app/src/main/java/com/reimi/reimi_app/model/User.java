f213dedf0d44aeda5c75462b0e4405b1f1652489
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-15 18:34:54 +0900

    feat: Userエンティティ作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/model/User.java
@@ -0,0 +1,27 @@
+package com.reimi.reimi_app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable= false)
+    private int id;
+    @NotBlank(message = "Title must not be blank.")
+    @Column(name = "name", nullable= false)
+    private String name;
+}
