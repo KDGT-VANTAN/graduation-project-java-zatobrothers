b3d68faf929eaca0574f366048261404244fe210
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-12-17 21:55:02 +0900

    feat: メイン写真を表示させるカードウィジェット作成

---
+++ reimi/frontend/reimi_app/lib/presentation/pages/profile/components/main_photo_card.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MainPhotoCard extends StatelessWidget {
+  const MainPhotoCard({
+    super.key,
+    required this.image,
+    required this.onTap,
+  });
+  final ImageProvider<Object> image;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(2),
+        decoration: BoxDecoration(
+          color: Colors.white.withValues(alpha: 0.9),
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(
+            color: Colors.white,
+            width: 2,
+          ),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(16),
+          child: AspectRatio(
+            // 正方形
+            aspectRatio: 1,
+            child: Image(
+              image: image,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
