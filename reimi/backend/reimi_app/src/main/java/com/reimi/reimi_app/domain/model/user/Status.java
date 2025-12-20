e71b320d840bc7f3b32a4b97538f8aadb343693a
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-20 19:08:11 +0900

    feat: アカウントの状態にラベル追加

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/model/user/Status.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/model/user/Status.java
@@ -1,8 +1,18 @@
 package com.reimi.reimi_app.domain.model.user;
 
 public enum Status {
-    ACTIVE,
-    WITHDRAWN,
-    BANNED,
-    SUSPENDED
+    ACTIVE("通常利用中"),
+    WITHDRAWN("自主退会"),
+    BANNED("強制退会"),
+    SUSPENDED("一時停止");
+
+    private final String label;
+
+    Status(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
