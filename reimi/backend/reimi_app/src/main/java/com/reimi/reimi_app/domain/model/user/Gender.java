d8e4302a077785e6ddb4247511e46a44c89765fe
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-20 19:06:00 +0900

    feat: 性別にラベル追加

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/model/user/Gender.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/model/user/Gender.java
@@ -1,7 +1,17 @@
 package com.reimi.reimi_app.domain.model.user;
 
 public enum Gender {
-    MAN,
-    WOMAN,
-    OTHER
+    MAN("男性"),
+    WOMAN("女性"),
+    OTHER("その他");
+
+    private final String label;
+
+    Gender(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
