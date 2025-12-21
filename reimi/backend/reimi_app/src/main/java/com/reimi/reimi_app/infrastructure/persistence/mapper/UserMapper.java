b6f99ac3ca8723948c0b8e25824a21363e6642f2
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-21 20:23:11 +0900

    feat: UserMapperの作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package com.reimi.reimi_app.infrastructure.persistence.mapper;
+
+import com.reimi.reimi_app.domain.model.user.User;
+import com.reimi.reimi_app.infrastructure.persistence.user.UserEntity;
+
+public class UserMapper {
+
+    public static UserEntity toEntity(User user) {
+        UserEntity entity = new UserEntity();
+        entity.setId(user.getId());
+        entity.setFirebaseUid(user.getFirebaseUid());
+        entity.setName(user.getName());
+        entity.setGender(user.getGender());
+        entity.setBirthDate(user.getBirthDate());
+        entity.setAddress(user.getAddress());
+        entity.setMainPhotoUrl(user.getMainPhotoUrl());
+        entity.setEmail(user.getEmail());
+        entity.setStatus(user.getStatus());
+
+        return entity;
+    }
+}
