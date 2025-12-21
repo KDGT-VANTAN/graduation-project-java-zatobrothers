e62a1d9ecee6cb89966eb1d1c50809cf8c70c6c3
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-12-21 21:06:25 +0900

    feat: カレントユーザーを取得するユースケース作成

---
+++ reimi/frontend/reimi_app/lib/domain/usecases/get_current_user_usecase.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import '../../domain/entities/user_entity.dart';
+import '../../domain/repositories/auth_repository.dart';
+import '../../domain/repositories/user_repository.dart';
+
+class GetCurrentUserUseCase {
+  const GetCurrentUserUseCase({
+    required this.authRepository,
+    required this.userRepository,
+  });
+  final AuthRepository authRepository;
+  final UserRepository userRepository;
+
+  Future<UserEntity?> call() async {
+    final user = await authRepository.getCurrentUser();
+    if (user == null) {
+      return null;
+    } 
+    // TODO: DBにデータがないので仮実装
+    else if (user.uid == dotenv.get('FIREBASE_UID')) {
+      return userRepository.fetchUser("user_000");
+    }
+    return userRepository.fetchUser(user.uid);
+  }
+}
