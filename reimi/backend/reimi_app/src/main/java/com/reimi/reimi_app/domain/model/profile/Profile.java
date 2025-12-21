97a1d75b91d160fa2f730ffd7764020819daaa7e
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-22 04:41:46 +0900

    feat: Profileのドメインモデル修正

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/model/profile/Profile.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/model/profile/Profile.java
@@ -4,19 +4,22 @@ import com.reimi.reimi_app.domain.model.user.UserId;
 
 public class Profile {
 
-    private final UserId user_id;
+    private final UserId userId;
 
     private final String introduction;
 
-    private Profile(UserId user_id, String introduction) {
-        this.user_id = user_id;
+    private Profile(UserId userId, String introduction) {
+        if (introduction == null || introduction.length() < 20 || introduction.length() > 500) {
+            throw new IllegalArgumentException("自己紹介文は20〜500文字である必要があります");
+        }
+        this.userId = userId;
         this.introduction = introduction;
     }
-    public static Profile create(UserId user_id, String introduction) {
-        return new Profile(user_id, introduction);
+    public static Profile create(UserId userId, String introduction) {
+        return new Profile(userId, introduction);
     }
 
-    public UserId getUserId() { return user_id; }
+    public UserId getUserId() { return userId; }
     public String getIntroduction() { return introduction; }
 
 }
