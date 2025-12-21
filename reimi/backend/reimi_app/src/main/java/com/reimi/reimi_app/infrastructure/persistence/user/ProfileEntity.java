c3799697fd2de04da34db7166a79996ef5833bcf
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-22 03:22:22 +0900

    feat: Profileエンティティを作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/user/ProfileEntity.java
@@ -0,0 +1,51 @@
+package com.reimi.reimi_app.infrastructure.persistence.user;
+
+import java.time.OffsetDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.reimi.reimi_app.domain.model.user.UserId;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "profiles")
+@EntityListeners(AuditingEntityListener.class)
+public class ProfileEntity {
+
+    @Id
+    @Column(name = "user_id")
+    private UserId userId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @Column(name = "introduction", nullable = false, length = 500)
+    private String introduction;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private OffsetDateTime updatedAt;
+
+    public ProfileEntity() {}
+}
