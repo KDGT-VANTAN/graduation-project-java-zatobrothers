3a64e832b8fcc8076369400c78511ca57acabf69
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-21 16:41:17 +0900

    feat: Userのコントローラ作成

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/UserController.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/UserController.java
@@ -1,28 +1,38 @@
-package com.reimi.reimi_app.controller;
+package com.reimi.reimi_app.infrastructure.web.controller;
 
-import java.net.URI;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.reimi.reimi_app.model.User;
-import com.reimi.reimi_app.service.UserService;
-
+import com.reimi.reimi_app.application.command.RegisterUserCommand;
+import com.reimi.reimi_app.application.usecase.UserUseCase;
+import com.reimi.reimi_app.domain.model.user.Address;
+import com.reimi.reimi_app.domain.model.user.Gender;
+import com.reimi.reimi_app.infrastructure.web.dto.request.RegisterUserRequest;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
-    @Autowired
-    private UserService _userService;
 
-    // User新規作成
-    @PostMapping(path="")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        User newUser = _userService.createUser(user);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
-        return ResponseEntity.created(location).body(newUser);
+    private final UserUseCase userUseCase;
+
+    public UserController(UserUseCase userUseCase) {
+        this.userUseCase = userUseCase;
+    }
+
+    @PostMapping
+    public void registerUser(@RequestBody RegisterUserRequest request) {
+        userUseCase.registerUser(
+            new RegisterUserCommand(
+                request.firebaseUid(),
+                request.name(),
+                Gender.valueOf(request.gender()),
+                request.birthDate(),
+                Address.valueOf(request.address()),
+                request.mainPhotoUrl(),
+                request.email()
+            )
+        );
     }
 }
