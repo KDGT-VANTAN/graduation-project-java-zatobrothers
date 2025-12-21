2d521816fafa33d48cb8cd2a423bac42f387565f
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-21 21:06:23 +0900

    feat: JPA Auditing機能が使える様に設定ファイルを作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.reimi.reimi_app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
