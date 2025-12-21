418b9040493e8e47309ba053fc230b4e6d1503d9
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-21 20:11:03 +0900

    feat: Userエンティティの全カラムにname属性を指定

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/user/UserEntity.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/user/UserEntity.java
@@ -30,31 +30,31 @@ public class UserEntity {
     @Column(name = "firebase_uid", nullable = false)
     private String firebaseUid;
 
-    @Column(nullable = false, length = 64)
+    @Column(name = "name", nullable = false, length = 64)
     private String name;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 10)
+    @Column(name = "gender", nullable = false, length = 10)
     private Gender gender;
 
     @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 64)
+    @Column(name = "address", nullable = false, length = 64)
     private Address address;
 
     @Column(name = "main_photo_url", nullable = false, length = 255)
     private String mainPhotoUrl;
 
-    @Column(nullable = false, length = 255)
+    @Column(name = "email", nullable = false, length = 255)
     private String email;
 
     @Column(name = "last_login_at")
     private OffsetDateTime lastLoginAt;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 32)
+    @Column(name = "status", nullable = false, length = 32)
     private Status status;
 
     @Column(name = "withdrawal_at")
@@ -68,4 +68,5 @@ public class UserEntity {
 
     public UserEntity() {
     }
+
 }
