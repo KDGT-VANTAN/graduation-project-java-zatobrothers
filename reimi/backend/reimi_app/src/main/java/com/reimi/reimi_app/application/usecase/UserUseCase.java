6a26b749c7346629ee88686f7a4827a2932263ee
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-23 02:24:59 +0900

    feat: ユースケースのインターフェイスにユーザー一覧取得の業務操作を追加

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/application/usecase/UserUseCase.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/application/usecase/UserUseCase.java
@@ -1,7 +1,13 @@
 package com.reimi.reimi_app.application.usecase;
 
+import java.util.List;
+
 import com.reimi.reimi_app.application.command.RegisterUserCommand;
+import com.reimi.reimi_app.domain.model.user.User;
 
 public interface UserUseCase {
+
+    List<User> getUsers();
+
     void registerUser(RegisterUserCommand command);
 }
