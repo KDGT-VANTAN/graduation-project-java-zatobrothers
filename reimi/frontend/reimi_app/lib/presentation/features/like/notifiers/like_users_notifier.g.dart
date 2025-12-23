cba3f6f8c961446e74a9c5ffbedaa5d0996f12df
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-12-23 10:00:00 +0900

    feat: いいね画面で表示するユーザー一覧を管理するプロバイダ作成

---
+++ reimi/frontend/reimi_app/lib/presentation/features/like/notifiers/like_users_notifier.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'like_users_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$likeUsersNotifierHash() => r'fc6b1f7e4cb3e0c8963cd95d878fb75994891af3';
+
+/// See also [LikeUsersNotifier].
+@ProviderFor(LikeUsersNotifier)
+final likeUsersNotifierProvider = AutoDisposeAsyncNotifierProvider<
+    LikeUsersNotifier, List<LikeUserModel>?>.internal(
+  LikeUsersNotifier.new,
+  name: r'likeUsersNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$likeUsersNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LikeUsersNotifier = AutoDisposeAsyncNotifier<List<LikeUserModel>?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
