db82a77b6dda5181ff6636cfbd0ebef723b28864
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-25 02:39:20 +0900

    feat: Userエンティティをドメインモデルに変換するメソッドを定義

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/mapper/UserMapper.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/mapper/UserMapper.java
@@ -1,9 +1,25 @@
 package com.reimi.reimi_app.infrastructure.persistence.mapper;
 
 import com.reimi.reimi_app.domain.model.user.User;
+import com.reimi.reimi_app.domain.model.user.UserId;
 import com.reimi.reimi_app.infrastructure.persistence.entity.UserEntity;
 
 public class UserMapper {
+
+    public static User toDomain(UserEntity entity) {
+        return User.reconstruct(
+                new UserId(entity.getId()),
+                entity.getFirebaseUid(),
+                entity.getName(),
+                entity.getGender(),
+                entity.getBirthDate(),
+                entity.getAddress(),
+                entity.getMainPhotoUrl(),
+                entity.getEmail(),
+                entity.getStatus(),
+                ProfileMapper.toDomain(entity.getProfile())
+        );
+    }
 
     public static UserEntity toEntity(User user) {
         UserEntity entity = new UserEntity();
