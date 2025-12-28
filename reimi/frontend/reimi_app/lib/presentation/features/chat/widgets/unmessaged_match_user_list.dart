9062912effa1a5a6ca3c2134aed06ffdec8ccce7
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-12-28 17:22:10 +0900

    feat: マッチングしたからまだメッセージのやり取りをしていないユーザー一覧を表示するウィジェット作成

---
+++ reimi/frontend/reimi_app/lib/presentation/features/chat/widgets/unmessaged_match_user_list.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:reimi_app/core/extensions/image_path_extension.dart';
+import 'package:reimi_app/data/models/unmessaged_match_user_model.dart';
+
+class UnmessagedMatchUserList extends StatelessWidget {
+  const UnmessagedMatchUserList({
+    super.key,
+    required this.users,
+    required this.onTap,
+  });
+  final List<UnmessagedMatchUserModel> users;
+  final void Function(String userId) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return SizedBox(
+      height: 86,
+      child: ListView.separated(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (_, index) {
+          return GestureDetector(
+            onTap: () {
+              onTap(users[index].userId);
+            },
+            child: Container(
+              width: 86,
+              height: 86,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: theme.colorScheme.secondary,
+                  width: 3,
+                ),
+                color: Colors.transparent,
+              ),
+              child: CircleAvatar(
+                backgroundColor: Colors.transparent,
+                child: CircleAvatar(
+                  radius: 38,
+                  backgroundImage: users[index].mainPhotoUrl.toImageProvider(),
+                ),
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (_, __) => const SizedBox(width: 8),
+        itemCount: users.length,
+      ),
+    );
+  }
+}
