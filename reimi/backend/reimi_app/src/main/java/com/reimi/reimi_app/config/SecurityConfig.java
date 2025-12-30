818266a52f4fbb859f3977577c4f7aed643eb609
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-30 17:17:44 +0900

    feat: CSRFを無効化

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/config/SecurityConfig.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable());
         return http.build();
     }
 }
