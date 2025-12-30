553316622bc2f1ad44b7264acbfb5f30d69a6491
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-30 22:49:01 +0900

    feat: コンポーネントにエラーハンドリング処理を追記

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/security/AuthenticatedUserProvider.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/security/AuthenticatedUserProvider.java
@@ -2,6 +2,8 @@ package com.reimi.reimi_app.security;
 
 import org.springframework.stereotype.Component;
 
+import com.reimi.reimi_app.application.exception.UnauthenticatedException;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -9,6 +11,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 public class AuthenticatedUserProvider {
     public String getFirebaseUid() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new UnauthenticatedException();
+        }
 
         return authentication.getName();
     }
