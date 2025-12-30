96f679d5de0ac6ac31fff5f7d991073eae46f45c
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-30 17:23:43 +0900

    feat: Firebase 認証用のフィルターを通る様にする

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/config/SecurityConfig.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/config/SecurityConfig.java
@@ -4,16 +4,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.reimi.reimi_app.security.FirebaseAuthenticationFilter;
+import com.reimi.reimi_app.security.FirebaseTokenVerifier;
+
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, FirebaseTokenVerifier verifier) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
             .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
+            .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+            .addFilterBefore(
+                new FirebaseAuthenticationFilter(verifier),
+                UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
