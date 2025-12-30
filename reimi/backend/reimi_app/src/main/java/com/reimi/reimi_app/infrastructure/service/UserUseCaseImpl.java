a9edf1740d94f7399e27b9320e819f6f10fc8138
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-30 21:59:25 +0900

    feat: UserユースケースにAuthenticatedUserProviderを注入

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/service/UserUseCaseImpl.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/service/UserUseCaseImpl.java
@@ -6,6 +6,7 @@ import com.reimi.reimi_app.application.exception.EmailAlreadyExistsException;
 import com.reimi.reimi_app.application.usecase.UserUseCase;
 import com.reimi.reimi_app.domain.model.user.User;
 import com.reimi.reimi_app.domain.repository.UserRepository;
+import com.reimi.reimi_app.security.AuthenticatedUserProvider;
 
 import java.util.List;
 
@@ -15,9 +16,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserUseCaseImpl implements UserUseCase {
 
+    private final AuthenticatedUserProvider authenticatedUserProvider;
     private final UserRepository userRepository;
 
-    public UserUseCaseImpl(UserRepository userRepository) {
+    public UserUseCaseImpl(
+        AuthenticatedUserProvider authenticatedUserProvider,
+        UserRepository userRepository
+    ) {
+        this.authenticatedUserProvider = authenticatedUserProvider;
         this.userRepository = userRepository;
     }
     @Override
