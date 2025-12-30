65cc9de729da50384da43872e96e253da7522624
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-30 23:28:06 +0900

    feat: ResponseDTOからfirebaseUidを直接送っているのを削除

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/UserController.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/UserController.java
@@ -44,7 +44,6 @@ public class UserController {
                 .stream()
                 .map(user -> new GetUserListResponse(
                     user.getId().value(),
-                    user.getFirebaseUid(),
                     user.getName(),
                     user.getBirthDate(),
                     user.getAddress().getLabel(),
