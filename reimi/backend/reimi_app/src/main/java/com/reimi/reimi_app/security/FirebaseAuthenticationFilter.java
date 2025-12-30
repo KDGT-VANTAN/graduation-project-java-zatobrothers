ae1f33ff4e021ed02bf049f46f59f5bdd5b64d3d
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-30 17:56:02 +0900

    feat: Firebase IDToken検証処理

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/security/FirebaseAuthenticationFilter.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/security/FirebaseAuthenticationFilter.java
@@ -2,7 +2,10 @@ package com.reimi.reimi_app.security;
 
 import java.io.IOException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.google.firebase.auth.FirebaseAuthException;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -11,6 +14,12 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class FirebaseAuthenticationFilter extends OncePerRequestFilter {
 
+    private final FirebaseTokenVerifier verifier;
+
+    public FirebaseAuthenticationFilter(FirebaseTokenVerifier verifier) {
+        this.verifier = verifier;
+    }
+
     @Override
     protected void doFilterInternal(
         HttpServletRequest request,
@@ -18,7 +27,19 @@ public class FirebaseAuthenticationFilter extends OncePerRequestFilter {
         FilterChain filterChain
     ) throws ServletException, IOException {
 
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+
+            try {
+                verifier.verify(token);
+
+            } catch (FirebaseAuthException e) {
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                return;
+            }
+        }
         filterChain.doFilter(request, response);
     }
-
 }
