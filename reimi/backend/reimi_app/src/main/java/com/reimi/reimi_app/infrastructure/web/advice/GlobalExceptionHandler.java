5d5af981b3b774e175c72c0158d09c7ba40ea4c4
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-31 23:34:57 +0900

    feat: 発生した全ての例外を一括して処理するクラスを作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/advice/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.reimi.reimi_app.infrastructure.web.advice;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
