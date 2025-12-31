2e5a75eb9e41defec4f7f6c0fc69b6bbc0ed8715
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-01 00:26:25 +0900

    feat: リソース不存在（404）の例外処理を作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/application/exception/client/ResourceNotFoundException.java
@@ -0,0 +1,15 @@
+package com.reimi.reimi_app.application.exception.client;
+
+import org.springframework.http.HttpStatus;
+
+import com.reimi.reimi_app.application.exception.ClientErrorException;
+
+public class ResourceNotFoundException extends ClientErrorException {
+    public ResourceNotFoundException(String resourceName) {
+        super(
+            "RESOURCE_NOT_FOUND",
+            resourceName + " が見つかりません",
+            HttpStatus.NOT_FOUND
+        );
+    }
+}
