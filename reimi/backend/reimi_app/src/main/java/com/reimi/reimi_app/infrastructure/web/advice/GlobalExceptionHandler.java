eaa1042bd6154b8259e8b6b7fdfa1395ced19b37
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-31 23:36:30 +0900

    feat: クライアント起因のエラー（400番台のエラー）処理

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/advice/GlobalExceptionHandler.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/advice/GlobalExceptionHandler.java
@@ -1,7 +1,23 @@
 package com.reimi.reimi_app.infrastructure.web.advice;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.reimi.reimi_app.application.exception.ClientErrorException;
+import com.reimi.reimi_app.infrastructure.web.dto.response.ApiErrorResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    // クライアント起因のエラー（400番台のエラー）処理
+    @ExceptionHandler(ClientErrorException.class)
+    public ResponseEntity<ApiErrorResponse> handleClientError(ClientErrorException ex) {
+        return ResponseEntity
+            .status(ex.getStatus())
+            .body(new ApiErrorResponse(
+                ex.getCode(),
+                ex.getMessage(),
+                null
+            ));
+    }
 }
