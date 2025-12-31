1120b61f20e405bb5adc360beb401de1a17eeddd
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-12-31 23:25:23 +0900

    feat: 不要な引数を消した

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/application/exception/EmailAlreadyExistsException.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/application/exception/EmailAlreadyExistsException.java
@@ -3,7 +3,7 @@ package com.reimi.reimi_app.application.exception;
 import org.springframework.http.HttpStatus;
 
 public class EmailAlreadyExistsException extends ClientErrorException {
-    public EmailAlreadyExistsException(String email) {
+    public EmailAlreadyExistsException() {
         super(
             "EMAIL_ALREADY_EXISTS",
             "既に使用されているメールアドレスです",
