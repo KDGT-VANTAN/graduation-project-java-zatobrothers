0ebbfa9acdb4448e7bd749cd763f64ad7ded735d
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-01 19:10:51 +0900

    feat: ウェザーリポート関連のenumの表示名用のextension作成

---
+++ reimi/frontend/reimi_app/lib/core/extensions/value_objects/wind_direction_extension.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:reimi_app/domain/value_objects/wind_direction.dart';
+import 'package:reimi_app/i18n/strings.g.dart';
+
+extension WindDirectionExtension on WindDirection {
+  String displayName(BuildContext context) {
+    final t = Translations.of(context).kEnum.windDirection;
+    switch (this) {
+      case WindDirection.north:
+        return t.north;
+      case WindDirection.northEast:
+        return t.northEast;
+      case WindDirection.east:
+        return t.east;
+      case WindDirection.southEast:
+        return t.southEast;
+      case WindDirection.south:
+        return t.south;
+      case WindDirection.southWest:
+        return t.southWest;
+      case WindDirection.west:
+        return t.west;
+      case WindDirection.northWest:
+        return t.northWest;
+      case WindDirection.unknown:
+        return t.unknown;
+    }
+  }
+}
