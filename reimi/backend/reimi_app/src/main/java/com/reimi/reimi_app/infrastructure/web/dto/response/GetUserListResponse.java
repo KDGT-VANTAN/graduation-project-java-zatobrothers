02d7ff50de92e67c0bde0e9955ec5c755f0319b4
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-03 07:44:43 +0900

    feat: type属性をやめる

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/dto/response/GetUserListResponse.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/dto/response/GetUserListResponse.java
@@ -8,21 +8,21 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "ユーザー一覧取得用レスポンス")
 public record GetUserListResponse (
 
-        @Schema(description = "ユーザーID", type = "string", example = "5bf5eb52-c5fb-4a4c-b6e3-25e53c28bf93")
+        @Schema(description = "ユーザーID", example = "5bf5eb52-c5fb-4a4c-b6e3-25e53c28bf93")
         UUID id,
 
-        @Schema(description = "名前", type = "string", example = "山田 太郎")
+        @Schema(description = "名前", example = "山田 太郎")
         String name,
 
-        @Schema(description = "生年月日", type = "LocalDate", example = "1996-04-18")
+        @Schema(description = "生年月日", example = "1996-04-18")
         LocalDate birthDate,
 
-        @Schema(description = "居住地", type = "string", example = "東京都")
+        @Schema(description = "居住地", example = "東京都")
         String address,
 
-        @Schema(description = "メイン写真の保存URL", type = "string", example = "images/users/main_12345.jpg")
+        @Schema(description = "メイン写真の保存URL", example = "images/users/main_12345.jpg")
         String mainPhotoUrl,
 
-        @Schema(description = "自己紹介文", type = "string", example = "都内でエンジニアをしています。休日はカフェ巡りやランニングを楽しんでいます。")
+        @Schema(description = "自己紹介文", example = "都内でエンジニアをしています。休日はカフェ巡りやランニングを楽しんでいます。")
         String introduction
 ) {}
