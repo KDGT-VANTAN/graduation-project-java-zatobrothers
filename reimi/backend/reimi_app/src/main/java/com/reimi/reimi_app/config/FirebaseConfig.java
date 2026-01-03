0ab0cac1bc07e7ff4dd131942fe5bb77f7e00eb8
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-04 01:46:53 +0900

    feat: デフォルトバケットをBeanで定義

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/config/FirebaseConfig.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/config/FirebaseConfig.java
@@ -9,10 +9,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
-
+import com.google.firebase.cloud.StorageClient;
 
 import org.springframework.core.io.Resource;
 
@@ -45,5 +46,10 @@ public class FirebaseConfig {
     @Bean
     public FirebaseAuth firebaseAuth(FirebaseApp firebaseApp) {
         return FirebaseAuth.getInstance(firebaseApp);
+    }
+
+    @Bean
+    public Bucket firebaseStorageBucket(FirebaseApp firebaseApp) {
+        return StorageClient.getInstance(firebaseApp).bucket();
     }
 }
