43f1e7ce5b1712fca1d909ea93e68ec85995e80c
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-05 02:15:00 +0900

    feat: user関連のapiを実装(fetchCurrentUserは後ほど対応)

--- reimi/frontend/reimi_app/lib/data/datasources/user_remote_datasource.dart
+++ reimi/frontend/reimi_app/lib/data/datasources/user_remote_datasource.dart
@@ -1,20 +1,44 @@
-import 'package:reimi_app/domain/entities/user_entity.dart';
+import 'package:dio/dio.dart';
+import 'package:reimi_app/data/models/app_user_model.dart';
+import 'package:reimi_app/data/models/home_user_model.dart';
+import 'package:reimi_app/data/models/user_registration_model.dart';
 
 abstract class UserRemoteDataSource {
-  Future<List<UserEntity>?> fetchUsers();
-  Future<UserEntity?> fetchUser(String userId);
+  Future<List<HomeUserModel>> fetchUsers();
+  Future<AppUserModel?> fetchCurrentUser();
+  Future<bool> createUser(UserRegistrationModel user);
 }
 
 class UserRemoteDataSourceImpl implements UserRemoteDataSource {
-  const UserRemoteDataSourceImpl();
+  const UserRemoteDataSourceImpl(this._dio);
+  final Dio _dio;
 
   @override
-  Future<List<UserEntity>?> fetchUsers() async {
-    return [];
+  Future<List<HomeUserModel>> fetchUsers() async {
+    final response = await _dio.get('/users');
+    final List<HomeUserModel> users =
+        response.data.map((res) => HomeUserModel.fromJson(res)).toList();
+    return users;
   }
 
   @override
-  Future<UserEntity?> fetchUser(String userId) async {
-    return null;
+  Future<AppUserModel?> fetchCurrentUser() async {
+    // null → 初回ユーザー
+    // return null;
+    return mockUser;
+  }
+
+  @override
+  Future<bool> createUser(UserRegistrationModel user) async {
+    final response = await _dio.post(
+      '/users',
+      data: user.toJson(),
+    );
+    if (response.statusCode == 201) {
+      return true;
+    }
+    return false;
   }
 }
+
+const mockUser = AppUserModel(id: 'user_000', name: 'メンダコ');
