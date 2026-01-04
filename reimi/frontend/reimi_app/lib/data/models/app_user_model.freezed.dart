8263b70e60075206c1af01d5b79133fe3236b80d
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-05 02:18:44 +0900

    feat: dbにユーザー情報があるか確認する用のユーザーモデル作成

---
+++ reimi/frontend/reimi_app/lib/data/models/app_user_model.freezed.dart
@@ -0,0 +1,330 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'app_user_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+// dart format off
+T _$identity<T>(T value) => value;
+
+/// @nodoc
+mixin _$AppUserModel {
+  String get id;
+  String get name;
+
+  /// Create a copy of AppUserModel
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @pragma('vm:prefer-inline')
+  $AppUserModelCopyWith<AppUserModel> get copyWith =>
+      _$AppUserModelCopyWithImpl<AppUserModel>(
+          this as AppUserModel, _$identity);
+
+  /// Serializes this AppUserModel to a JSON map.
+  Map<String, dynamic> toJson();
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is AppUserModel &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, name);
+
+  @override
+  String toString() {
+    return 'AppUserModel(id: $id, name: $name)';
+  }
+}
+
+/// @nodoc
+abstract mixin class $AppUserModelCopyWith<$Res> {
+  factory $AppUserModelCopyWith(
+          AppUserModel value, $Res Function(AppUserModel) _then) =
+      _$AppUserModelCopyWithImpl;
+  @useResult
+  $Res call({String id, String name});
+}
+
+/// @nodoc
+class _$AppUserModelCopyWithImpl<$Res> implements $AppUserModelCopyWith<$Res> {
+  _$AppUserModelCopyWithImpl(this._self, this._then);
+
+  final AppUserModel _self;
+  final $Res Function(AppUserModel) _then;
+
+  /// Create a copy of AppUserModel
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+  }) {
+    return _then(_self.copyWith(
+      id: null == id
+          ? _self.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _self.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// Adds pattern-matching-related methods to [AppUserModel].
+extension AppUserModelPatterns on AppUserModel {
+  /// A variant of `map` that fallback to returning `orElse`.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case _:
+  ///     return orElse();
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>(
+    TResult Function(_AppUserModel value)? $default, {
+    required TResult orElse(),
+  }) {
+    final _that = this;
+    switch (_that) {
+      case _AppUserModel() when $default != null:
+        return $default(_that);
+      case _:
+        return orElse();
+    }
+  }
+
+  /// A `switch`-like method, using callbacks.
+  ///
+  /// Callbacks receives the raw object, upcasted.
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case final Subclass2 value:
+  ///     return ...;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>(
+    TResult Function(_AppUserModel value) $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _AppUserModel():
+        return $default(_that);
+      case _:
+        throw StateError('Unexpected subclass');
+    }
+  }
+
+  /// A variant of `map` that fallback to returning `null`.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case _:
+  ///     return null;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>(
+    TResult? Function(_AppUserModel value)? $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _AppUserModel() when $default != null:
+        return $default(_that);
+      case _:
+        return null;
+    }
+  }
+
+  /// A variant of `when` that fallback to an `orElse` callback.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case _:
+  ///     return orElse();
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>(
+    TResult Function(String id, String name)? $default, {
+    required TResult orElse(),
+  }) {
+    final _that = this;
+    switch (_that) {
+      case _AppUserModel() when $default != null:
+        return $default(_that.id, _that.name);
+      case _:
+        return orElse();
+    }
+  }
+
+  /// A `switch`-like method, using callbacks.
+  ///
+  /// As opposed to `map`, this offers destructuring.
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case Subclass2(:final field2):
+  ///     return ...;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>(
+    TResult Function(String id, String name) $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _AppUserModel():
+        return $default(_that.id, _that.name);
+      case _:
+        throw StateError('Unexpected subclass');
+    }
+  }
+
+  /// A variant of `when` that fallback to returning `null`
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case _:
+  ///     return null;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>(
+    TResult? Function(String id, String name)? $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _AppUserModel() when $default != null:
+        return $default(_that.id, _that.name);
+      case _:
+        return null;
+    }
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _AppUserModel implements AppUserModel {
+  const _AppUserModel({required this.id, required this.name});
+  factory _AppUserModel.fromJson(Map<String, dynamic> json) =>
+      _$AppUserModelFromJson(json);
+
+  @override
+  final String id;
+  @override
+  final String name;
+
+  /// Create a copy of AppUserModel
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @pragma('vm:prefer-inline')
+  _$AppUserModelCopyWith<_AppUserModel> get copyWith =>
+      __$AppUserModelCopyWithImpl<_AppUserModel>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$AppUserModelToJson(
+      this,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _AppUserModel &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, name);
+
+  @override
+  String toString() {
+    return 'AppUserModel(id: $id, name: $name)';
+  }
+}
+
+/// @nodoc
+abstract mixin class _$AppUserModelCopyWith<$Res>
+    implements $AppUserModelCopyWith<$Res> {
+  factory _$AppUserModelCopyWith(
+          _AppUserModel value, $Res Function(_AppUserModel) _then) =
+      __$AppUserModelCopyWithImpl;
+  @override
+  @useResult
+  $Res call({String id, String name});
+}
+
+/// @nodoc
+class __$AppUserModelCopyWithImpl<$Res>
+    implements _$AppUserModelCopyWith<$Res> {
+  __$AppUserModelCopyWithImpl(this._self, this._then);
+
+  final _AppUserModel _self;
+  final $Res Function(_AppUserModel) _then;
+
+  /// Create a copy of AppUserModel
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @pragma('vm:prefer-inline')
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+  }) {
+    return _then(_AppUserModel(
+      id: null == id
+          ? _self.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _self.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+// dart format on
