3a3a8f7cc6bd50a7318c8072ebff40eb1f5c0cac
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-05 02:41:35 +0900

    fix: envファイルがいらなくなったので、それに伴う修正

--- reimi/frontend/reimi_app/lib/main.dart
+++ reimi/frontend/reimi_app/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:reimi_app/core/config/crashlytics_config.dart';
 import 'package:reimi_app/core/config/firebase_options_dev.dart' as dev;
@@ -13,7 +12,6 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: dev.DefaultFirebaseOptions.currentPlatform,
   );
-  await dotenv.load(fileName: '.env');
   setupCrashlytics();
   runApp(
     ProviderScope(
