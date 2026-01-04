748436a556d8db40ef4dd225e1c9fdced3b710f6
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-05 05:26:50 +0900

    feat: リポジトリのインターフェイスにユーザー取得の操作を追加

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/repository/UserRepository.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.reimi.reimi_app.domain.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.reimi.reimi_app.domain.model.user.User;
 
 public interface UserRepository {
+
+    Optional<User> findMeByFirebaseUid(String firebaseUid);
 
     List<User> findAllUserExcludingMeByFirebaseUid(String firebaseUid);
 
