38f11a268649646d636ae6461f252242fb5df0b3
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-06 21:32:55 +0900

    feat: 報酬アイテムクラスを仮作成

---
+++ reimi/frontend/reimi_app/lib/data/models/reward_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class RewardItem {
+  const RewardItem({
+    required this.icon,
+    required this.amountText,
+  });
+
+  final Widget icon;
+  final String amountText;
+}
