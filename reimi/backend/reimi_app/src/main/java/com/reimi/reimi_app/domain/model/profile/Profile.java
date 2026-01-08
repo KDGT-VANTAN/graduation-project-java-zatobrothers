5303bdc253c852bbfb340dbf6402356f4753479b
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-09 04:09:21 +0900

    feat: Profileが作成されたときに値が必須でないカラムはnullにする

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/model/profile/Profile.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/model/profile/Profile.java
@@ -1,19 +1,63 @@
 package com.reimi.reimi_app.domain.model.profile;
 
+import java.util.List;
+
 import com.reimi.reimi_app.domain.model.user.UserId;
 
 public class Profile {
 
     private final UserId userId;
-
     private final String introduction;
+    private final Height height;
+    private final BodyShape bodyShape;
+    private final AnnualIncome annualIncome;
+    private final BloodType bloodType;
+    private final Hometown hometown;
+    private final CommunicationStyle communicationStyle;
+    private final Occupation occupation;
+    private final Education education;
+    private final Smoking smoking;
+    private final Alcohol alcohol;
+    private final Holiday holiday;
+    private final List<String> sunnyDayHobbies;
+    private final List<String> rainyDayHobbies;
 
-    private Profile(UserId userId, String introduction) {
+
+    private Profile(
+        UserId userId,
+        String introduction,
+        Height height,
+        BodyShape bodyShape,
+        AnnualIncome annualIncome,
+        BloodType bloodType,
+        Hometown hometown,
+        CommunicationStyle communicationStyle,
+        Occupation occupation,
+        Education education,
+        Smoking smoking,
+        Alcohol alcohol,
+        Holiday holiday,
+        List<String> sunnyDayHobbies,
+        List<String> rainyDayHobbies
+    ) {
         if (introduction == null || introduction.length() < 20 || introduction.length() > 500) {
             throw new IllegalArgumentException("自己紹介文は20〜500文字である必要があります");
         }
         this.userId = userId;
         this.introduction = introduction;
+        this.height = height;
+        this.bodyShape = bodyShape;
+        this.annualIncome = annualIncome;
+        this.bloodType = bloodType;
+        this.hometown = hometown;
+        this.communicationStyle = communicationStyle;
+        this.occupation = occupation;
+        this.education = education;
+        this.smoking = smoking;
+        this.alcohol = alcohol;
+        this.holiday = holiday;
+        this.sunnyDayHobbies = sunnyDayHobbies;
+        this.rainyDayHobbies = rainyDayHobbies;
     }
     public static Profile create(
         UserId userId,
@@ -21,21 +65,59 @@ public class Profile {
     ) {
         return new Profile(
             userId,
-            introduction
+            introduction,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
         );
     }
 
     public static Profile reconstruct(
         UserId userId,
-        String introduction
+        String introduction,
+        Height height,
+        BodyShape bodyShape,
+        AnnualIncome annualIncome,
+        BloodType bloodType,
+        Hometown hometown,
+        CommunicationStyle communicationStyle,
+        Occupation occupation,
+        Education education,
+        Smoking smoking,
+        Alcohol alcohol,
+        Holiday holiday,
+        List<String> sunnyDayHobbies,
+        List<String> rainyDayHobbies
     ) {
         return new Profile(
                 userId,
-                introduction
+                introduction,
+                height,
+                bodyShape,
+                annualIncome,
+                bloodType,
+                hometown,
+                communicationStyle,
+                occupation,
+                education,
+                smoking,
+                alcohol,
+                holiday,
+                sunnyDayHobbies,
+                rainyDayHobbies
         );
     }
 
     public UserId getUserId() { return userId; }
     public String getIntroduction() { return introduction; }
-
 }
