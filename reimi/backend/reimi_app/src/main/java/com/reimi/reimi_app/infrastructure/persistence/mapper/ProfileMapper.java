fe6ed6ae4d40dae794c4a73492570a2519364023
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-10 04:16:04 +0900

    feat: 追加したカラムもドメイン・エンティティ変換時にセットされる様にする

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/mapper/ProfileMapper.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/mapper/ProfileMapper.java
@@ -10,13 +10,39 @@ public class ProfileMapper {
     public static Profile toDomain(ProfileEntity entity) {
         return Profile.reconstruct(
                 new UserId(entity.getUserId()),
-                entity.getIntroduction()
+                entity.getIntroduction(),
+                entity.getHeight(),
+                entity.getBodyShape(),
+                entity.getAnnualIncome(),
+                entity.getBloodType(),
+                entity.getHometown(),
+                entity.getCommunicationStyle(),
+                entity.getOccupation(),
+                entity.getEducation(),
+                entity.getSmoking(),
+                entity.getAlcohol(),
+                entity.getHoliday(),
+                entity.getSunnyDayHobbies(),
+                entity.getRainyDayHobbies()
         );
     }
     public static ProfileEntity toEntity(Profile profile, UserEntity userEntity) {
         ProfileEntity entity = new ProfileEntity();
         entity.setUser(userEntity);
         entity.setIntroduction(profile.getIntroduction());
+        entity.setHeight(profile.getHeight());
+        entity.setBodyShape(profile.getBodyShape());
+        entity.setAnnualIncome(profile.getAnnualIncome());
+        entity.setBloodType(profile.getBloodType());
+        entity.setHometown(profile.getHometown());
+        entity.setCommunicationStyle(profile.getCommunicationStyle());
+        entity.setOccupation(profile.getOccupation());
+        entity.setEducation(profile.getEducation());
+        entity.setSmoking(profile.getSmoking());
+        entity.setAlcohol(profile.getAlcohol());
+        entity.setHoliday(profile.getHoliday());
+        entity.setSunnyDayHobbies(profile.getSunnyDayHobbies());
+        entity.setRainyDayHobbies(profile.getRainyDayHobbies());
 
         return entity;
     }
