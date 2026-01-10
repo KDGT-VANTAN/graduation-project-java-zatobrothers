3cc2503b61799855eb0a2a2fb93f063d32765308
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-10 11:17:19 +0900

    feat: EntityのGetterでUUIDからUserIDに型変換を行う

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/entity/UserEntity.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/entity/UserEntity.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 import com.reimi.reimi_app.domain.model.user.Address;
 import com.reimi.reimi_app.domain.model.user.Gender;
 import com.reimi.reimi_app.domain.model.user.Status;
+import com.reimi.reimi_app.domain.model.user.UserId;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -96,6 +97,10 @@ public class UserEntity {
     @PreUpdate
     protected void preUpdate() {
         this.updatedAt = OffsetDateTime.now(ZoneOffset.UTC);
+    }
+
+    public UserId getId() {
+        return new UserId(id);
     }
 
     public UserEntity() {}
