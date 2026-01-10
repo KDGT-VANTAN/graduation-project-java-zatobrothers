df9249b4135aa79c8689f3d2a5bfa4bbfaaa7c1c
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-10 22:35:38 +0900

    feat: リクエストDTOを作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/dto/request/UpdateUserProfileRequest.java
@@ -0,0 +1,44 @@
+package com.reimi.reimi_app.infrastructure.web.dto.request;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.reimi.reimi_app.domain.model.profile.Alcohol;
+import com.reimi.reimi_app.domain.model.profile.AnnualIncome;
+import com.reimi.reimi_app.domain.model.profile.BloodType;
+import com.reimi.reimi_app.domain.model.profile.BodyShape;
+import com.reimi.reimi_app.domain.model.profile.CommunicationStyle;
+import com.reimi.reimi_app.domain.model.profile.Education;
+import com.reimi.reimi_app.domain.model.profile.Holiday;
+import com.reimi.reimi_app.domain.model.profile.Hometown;
+import com.reimi.reimi_app.domain.model.profile.Occupation;
+import com.reimi.reimi_app.domain.model.profile.Smoking;
+import com.reimi.reimi_app.domain.model.user.Address;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "ユーザープロフィール編集用リクエスト")
+public record UpdateUserProfileRequest (
+
+    UUID id,
+    String name,
+    Address address,
+    MultipartFile mainPhoto,
+    String introduction,
+    Integer height,
+    BodyShape bodyShape,
+    AnnualIncome annualIncome,
+    BloodType bloodType,
+    Hometown hometown,
+    CommunicationStyle communicationStyle,
+    Occupation occupation,
+    Education education,
+    Smoking smoking,
+    Alcohol alcohol,
+    Holiday holiday,
+    List<String> sunnyDayHobbies,
+    List<String> rainyDayHobbies
+
+) {}
