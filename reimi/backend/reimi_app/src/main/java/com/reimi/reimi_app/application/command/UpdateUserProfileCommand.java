7eb44574ceeb4dc6b917b457cc2ac15658039e18
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-10 22:35:51 +0900

    feat: Commandを作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/application/command/UpdateUserProfileCommand.java
@@ -0,0 +1,41 @@
+package com.reimi.reimi_app.application.command;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.reimi.reimi_app.domain.model.profile.Alcohol;
+import com.reimi.reimi_app.domain.model.profile.AnnualIncome;
+import com.reimi.reimi_app.domain.model.profile.BloodType;
+import com.reimi.reimi_app.domain.model.profile.BodyShape;
+import com.reimi.reimi_app.domain.model.profile.CommunicationStyle;
+import com.reimi.reimi_app.domain.model.profile.Education;
+import com.reimi.reimi_app.domain.model.profile.Holiday;
+import com.reimi.reimi_app.domain.model.profile.Hometown;
+import com.reimi.reimi_app.domain.model.profile.Occupation;
+import com.reimi.reimi_app.domain.model.profile.Smoking;
+import com.reimi.reimi_app.domain.model.user.Address;
+
+public record UpdateUserProfileCommand (
+
+    UUID id,
+    String name,
+    Address address,
+    MultipartFile mainPhoto,
+    String introduction,
+    Integer height,
+    BodyShape bodyShape,
+    AnnualIncome annualIncome,
+    BloodType bloodType,
+    Hometown hometown,
+    CommunicationStyle communicationStyle,
+    Occupation occupation,
+    Education education,
+    Smoking smoking,
+    Alcohol alcohol,
+    Holiday holiday,
+    List<String> sunnyDayHobbies,
+    List<String> rainyDayHobbies
+
+) {}
