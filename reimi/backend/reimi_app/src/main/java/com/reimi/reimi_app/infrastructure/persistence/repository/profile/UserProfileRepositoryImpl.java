79501a2e6c180a5f464679c756f600fd7ce0d99d
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-11 05:35:24 +0900

    feat: メソッドをオーバーライドしてUserとProfileのエンティティを保存する処理を追加

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/repository/profile/UserProfileRepositoryImpl.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/repository/profile/UserProfileRepositoryImpl.java
@@ -4,10 +4,13 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
+import com.reimi.reimi_app.application.exception.client.ResourceNotFoundException;
 import com.reimi.reimi_app.domain.model.profile.Profile;
 import com.reimi.reimi_app.domain.model.user.User;
 import com.reimi.reimi_app.domain.model.user.UserId;
 import com.reimi.reimi_app.domain.repository.UserProfileRepository;
+import com.reimi.reimi_app.infrastructure.persistence.entity.ProfileEntity;
+import com.reimi.reimi_app.infrastructure.persistence.entity.UserEntity;
 import com.reimi.reimi_app.infrastructure.persistence.mapper.ProfileMapper;
 import com.reimi.reimi_app.infrastructure.persistence.mapper.UserMapper;
 import com.reimi.reimi_app.infrastructure.persistence.repository.user.JpaUserRepository;
@@ -38,5 +41,23 @@ public class UserProfileRepositoryImpl implements UserProfileRepository {
         return jpaProfileRepository
             .findById(userId.value())
             .map(ProfileMapper::toDomain);
+    }
+
+    @Override
+    public void saveUser(User user) {
+        UserEntity entity = jpaUserRepository
+            .findById(user.getId().value())
+            .orElseThrow(() -> new ResourceNotFoundException("プロフィール"));
+        jpaUserRepository.save(UserMapper.toUpdateEntity(user, entity));
+    }
+
+    @Override
+    public void saveProfile(Profile profile) {
+
+        ProfileEntity entity = jpaProfileRepository
+            .findById(profile.getUserId().value())
+            .orElseThrow(() -> new ResourceNotFoundException("プロフィール"));
+
+        jpaProfileRepository.save(ProfileMapper.toUpdateEntity(profile, entity));
     }
 }
