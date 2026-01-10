318b5da828f4fdab8aba3a2a53c21ee452a6865d
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-11 05:12:39 +0900

    feat: ユーザープロフィール編集操作を実装

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/service/UserProfileUseCaseImpl.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/service/UserProfileUseCaseImpl.java
@@ -3,6 +3,8 @@ package com.reimi.reimi_app.infrastructure.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.reimi.reimi_app.application.command.UpdateUserProfileCommand;
+import com.reimi.reimi_app.application.exception.client.InvalidRequestException;
 import com.reimi.reimi_app.application.exception.client.ResourceNotFoundException;
 import com.reimi.reimi_app.application.usecase.UserProfileUseCase;
 import com.reimi.reimi_app.domain.model.profile.Profile;
@@ -10,6 +12,7 @@ import com.reimi.reimi_app.domain.model.user.User;
 import com.reimi.reimi_app.domain.model.user.UserId;
 import com.reimi.reimi_app.domain.repository.UserProfileRepository;
 import com.reimi.reimi_app.infrastructure.storage.image.ImageStorageComponent;
+import com.reimi.reimi_app.infrastructure.storage.image.ImageStoragePath;
 import com.reimi.reimi_app.infrastructure.web.dto.response.UserWithProfileResponse;
 
 @Service
@@ -17,14 +20,18 @@ public class UserProfileUseCaseImpl implements UserProfileUseCase {
 
     private final UserProfileRepository userProfileRepository;
     private final ImageStorageComponent imageStorage;
+    private final ImageStoragePath imageStoragePath;
 
     public UserProfileUseCaseImpl(
         UserProfileRepository userProfileRepository,
-        ImageStorageComponent imageStorage
+        ImageStorageComponent imageStorage,
+        ImageStoragePath imageStoragePath
     ) {
         this.userProfileRepository = userProfileRepository;
         this.imageStorage = imageStorage;
+        this.imageStoragePath = imageStoragePath;
     }
+
     @Override
     @Transactional(readOnly = true)
     public UserWithProfileResponse getUserProfile(UserId userId) {
@@ -59,5 +66,51 @@ public class UserProfileUseCaseImpl implements UserProfileUseCase {
             profile.getSunnyDayHobbies() != null ? profile.getSunnyDayHobbies() : null,
             profile.getRainyDayHobbies() != null ? profile.getRainyDayHobbies() : null
         );
+    }
+
+    @Override
+    @Transactional
+    public void updateUserProfile(UserId userId, UpdateUserProfileCommand command) {
+
+        if (command.mainPhoto() == null || command.mainPhoto().isEmpty()) {
+            throw new InvalidRequestException("メイン写真は必須です");
+        }
+
+        User user = userProfileRepository.findUserByUserId(userId)
+            .orElseThrow(() -> new ResourceNotFoundException("ユーザー"));
+
+        // メイン写真のベースパスを取得
+        String basePath = imageStoragePath.userMainPhotoPath();
+        // メイン写真の画像アップロード
+        String mainPhotoPath = imageStorage.imageUpload(command.mainPhoto(), basePath);
+
+        Profile profile = userProfileRepository.findProfileByUserId(userId)
+            .orElseThrow(() -> new ResourceNotFoundException("ユーザーのプロフィール"));
+
+        user.update(
+            command.name(),
+            command.address(),
+            mainPhotoPath
+        );
+
+        profile.update(
+            command.introduction(),
+            command.height(),
+            command.bodyShape(),
+            command.annualIncome(),
+            command.bloodType(),
+            command.hometown(),
+            command.communicationStyle(),
+            command.occupation(),
+            command.education(),
+            command.smoking(),
+            command.alcohol(),
+            command.holiday(),
+            command.sunnyDayHobbies(),
+            command.rainyDayHobbies()
+        );
+
+        userProfileRepository.saveUser(user);
+        userProfileRepository.saveProfile(profile);
     }
 }
