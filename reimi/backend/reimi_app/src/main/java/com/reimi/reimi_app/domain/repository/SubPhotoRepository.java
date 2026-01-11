bdc82e5351b4092c90814c011cd4d58d328db9ab
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-12 01:16:03 +0900

    feat: サブ写真のリポジトリインターフェイスに保存メソッドを定義

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/repository/SubPhotoRepository.java
@@ -0,0 +1,7 @@
+package com.reimi.reimi_app.domain.repository;
+
+import com.reimi.reimi_app.domain.model.subphoto.SubPhoto;
+
+public interface SubPhotoRepository {
+    void save(SubPhoto subPhoto);
+}
