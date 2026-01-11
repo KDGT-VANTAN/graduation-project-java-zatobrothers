a4a74ac8789e0fcc49df4f45c36dc0d625cb3bc9
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-12 06:23:27 +0900

    feat: ユーザープロフィールのサブ写真のパスを取得できるメソッドを追加

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/storage/image/ImageStoragePath.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/storage/image/ImageStoragePath.java
@@ -2,9 +2,17 @@ package com.reimi.reimi_app.infrastructure.storage.image;
 
 import org.springframework.stereotype.Component;
 
+import com.reimi.reimi_app.domain.model.profile.ProfileId;
+
 @Component
 public class ImageStoragePath {
+    // メイン写真のパス
     public String userMainPhotoPath() {
         return "images/users/mainPhoto";
+    }
+
+    // ユーザープロフィールのサブ写真のパス
+    public String userProfileSubPhotoPath(ProfileId profileId) {
+        return "images/users/profiles/" + profileId + "/subPhotos";
     }
 }
