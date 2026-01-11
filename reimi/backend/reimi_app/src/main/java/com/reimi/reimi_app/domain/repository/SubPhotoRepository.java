fe31fc6deef8242889580e84d6aa547aa0bda26e
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-12 06:21:43 +0900

    feat: 登録されているサブ写真取得の操作を定義

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/repository/SubPhotoRepository.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/repository/SubPhotoRepository.java
@@ -1,7 +1,13 @@
 package com.reimi.reimi_app.domain.repository;
 
+import java.util.List;
+
+import com.reimi.reimi_app.domain.model.profile.ProfileId;
 import com.reimi.reimi_app.domain.model.subphoto.SubPhoto;
 
 public interface SubPhotoRepository {
+
+    List<SubPhoto> findAllSubPhotos(ProfileId profileId);
+
     void save(SubPhoto subPhoto);
 }
