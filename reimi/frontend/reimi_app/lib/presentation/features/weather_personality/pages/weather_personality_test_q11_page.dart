f5453ea326be0a2af760da26f3369ad3a81dbe5a
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-13 00:41:55 +0900

    feat: ウェザーパーソナリティ診断の11問目の画面実装

---
+++ reimi/frontend/reimi_app/lib/presentation/features/weather_personality/pages/weather_personality_test_q11_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:reimi_app/i18n/strings.g.dart';
+import 'package:reimi_app/presentation/features/weather_personality/notifiers/weather_personality_test_notifier.dart';
+import 'package:reimi_app/presentation/features/weather_personality/pages/weather_personality_test_q12_page.dart';
+import 'package:reimi_app/presentation/features/weather_personality/widgets/weather_personality_test_page.dart';
+
+class WeatherPersonalityTestQ11Page extends ConsumerWidget {
+  const WeatherPersonalityTestQ11Page({super.key});
+
+  static String get routeName => 'weather_personality_test_q11';
+  static String get routeLocation => '/$routeName';
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final t = Translations.of(context);
+    final notifier = ref.read(weatherPersonalityTestNotifierProvider.notifier);
+    final q11Answer = ref.watch(weatherPersonalityTestNotifierProvider
+        .select((state) => state.q11Answer));
+
+    return WeatherPersonalityTestPage(
+      title: t.weatherPersonalityTestPage.q11.title,
+      question: t.weatherPersonalityTestPage.q11.question,
+      answerA: t.weatherPersonalityTestPage.q11.answerA,
+      answerB: t.weatherPersonalityTestPage.q11.answerB,
+      isSelected: q11Answer != null,
+      value: q11Answer,
+      onTapAnswerOption: (score) {
+        notifier.updateQ11Answer(score);
+      },
+      onPressedNext: () {
+        notifier.nextPage();
+        context.push(WeatherPersonalityTestQ12Page.routeLocation);
+      },
+    );
+  }
+}
