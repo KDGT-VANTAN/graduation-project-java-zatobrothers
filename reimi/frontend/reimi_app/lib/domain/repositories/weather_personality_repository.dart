a2cff33684dca5965b6f25980e586f84852471e2
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-13 00:08:26 +0900

    feat: WeatherPersonality関連の抽象クラスのrepositoryを実装

---
+++ reimi/frontend/reimi_app/lib/domain/repositories/weather_personality_repository.dart
@@ -0,0 +1,7 @@
+import 'package:reimi_app/domain/params/test_weather_personality_params.dart';
+import 'package:reimi_app/domain/read_models/weather_personality_result_read_model.dart';
+
+abstract class WeatherPersonalityRepository {
+  Future<void> testWeatherPersonality(TestWeatherPersonalityParams params);
+  Future<WeatherPersonalityResultReadModel> fetchWeatherPersonalityResult();
+}
