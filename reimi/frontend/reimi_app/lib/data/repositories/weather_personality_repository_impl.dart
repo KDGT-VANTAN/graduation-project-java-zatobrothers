609cb84446624053a1e01b22c4742a95c5d5fdb3
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-13 00:07:42 +0900

    feat: WeatherPersonality関連のrepositoryを実装

---
+++ reimi/frontend/reimi_app/lib/data/repositories/weather_personality_repository_impl.dart
@@ -0,0 +1,21 @@
+import 'package:reimi_app/data/datasources/remote/weather_personality_remote_datasource.dart';
+import 'package:reimi_app/data/extensions/test_weather_personality_dto_extension.dart';
+import 'package:reimi_app/domain/params/test_weather_personality_params.dart';
+import 'package:reimi_app/domain/read_models/weather_personality_result_read_model.dart';
+import 'package:reimi_app/domain/repositories/weather_personality_repository.dart';
+
+class WeatherPersonalityRepositoryImpl implements WeatherPersonalityRepository {
+  const WeatherPersonalityRepositoryImpl(this._remote);
+  final WeatherPersonalityRemoteDataSource _remote;
+
+  @override
+  Future<void> testWeatherPersonality(TestWeatherPersonalityParams params) {
+    final dto = params.toDto();
+    return _remote.testWeatherPersonality(dto);
+  }
+
+  @override
+  Future<WeatherPersonalityResultReadModel> fetchWeatherPersonalityResult() {
+    return _remote.fetchWeatherPersonalityResult();
+  }
+}
