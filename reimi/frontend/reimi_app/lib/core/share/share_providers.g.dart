426274e2ec33ead490e01e1ebfae5b93ba3720c9
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-13 20:35:15 +0900

    feat: 共有サービスのプロバイダー作成

---
+++ reimi/frontend/reimi_app/lib/core/share/share_providers.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'share_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$shareServiceHash() => r'93ac4ca96b3a631c994682810419716afa3205ed';
+
+/// See also [shareService].
+@ProviderFor(shareService)
+final shareServiceProvider = AutoDisposeProvider<ShareService>.internal(
+  shareService,
+  name: r'shareServiceProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$shareServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ShareServiceRef = AutoDisposeProviderRef<ShareService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
