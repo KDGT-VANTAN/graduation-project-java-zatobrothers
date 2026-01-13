05b2fa01ee331eedab79c0f3af0ccde4afc7f532
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-14 01:44:04 +0900

    feat: 自分がいいねした（された）ユーザー一覧取得用レスポンスDTO作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/dto/response/GetLikedUserListResponse.java
@@ -0,0 +1,27 @@
+package com.reimi.reimi_app.infrastructure.web.dto.response;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import com.reimi.reimi_app.domain.model.user.Address;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "自分がいいねした（された）ユーザー一覧取得用レスポンス")
+public record GetLikedUserListResponse (
+
+        @Schema(description = "ユーザーID", example = "5bf5eb52-c5fb-4a4c-b6e3-25e53c28bf93")
+        UUID id,
+
+        @Schema(description = "名前", example = "山田 太郎")
+        String name,
+
+        @Schema(description = "生年月日", example = "1996-04-18")
+        LocalDate birthDate,
+
+        @Schema(description = "居住地", example = "TOKYO")
+        Address address,
+
+        @Schema(description = "メイン写真URL")
+        String mainPhotoUrl
+) {}
