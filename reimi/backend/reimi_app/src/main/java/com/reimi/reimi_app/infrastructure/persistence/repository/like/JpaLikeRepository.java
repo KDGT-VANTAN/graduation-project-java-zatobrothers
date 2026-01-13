a7299fd702c7eb276d9485a94839b7ad2d8afd77
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-13 19:26:22 +0900

    feat: JPQLを用いてJPA Repositoryメソッド作成

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/repository/like/JpaLikeRepository.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/repository/like/JpaLikeRepository.java
@@ -1,11 +1,20 @@
 package com.reimi.reimi_app.infrastructure.persistence.repository.like;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.reimi.reimi_app.infrastructure.persistence.entity.LikeEntity;
 
 public interface JpaLikeRepository extends JpaRepository<LikeEntity, UUID> {
     boolean existsByFromUserIdAndToUserId(UUID fromUserId, UUID toUserId);
+
+    @Query("""
+        select l.toUserId
+        from LikeEntity l
+        where l.fromUserId = :fromUserId
+    """)
+    List<UUID> findToUserIdsByFromUserId(UUID fromUserId);
 }
