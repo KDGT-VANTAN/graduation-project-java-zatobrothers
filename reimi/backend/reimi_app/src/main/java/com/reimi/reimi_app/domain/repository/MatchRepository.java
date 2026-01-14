01f61fc0f491725540ff0e6490453e872fe77d71
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-15 01:26:33 +0900

    feat: マッチリポジトリのインターフェイスにメソッド追加

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/repository/MatchRepository.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/repository/MatchRepository.java
@@ -1,5 +1,7 @@
 package com.reimi.reimi_app.domain.repository;
 
+import java.util.List;
+
 import com.reimi.reimi_app.domain.model.match.Match;
 import com.reimi.reimi_app.domain.model.user.UserId;
 
@@ -8,4 +10,6 @@ public interface MatchRepository {
     boolean exists(UserId userAId, UserId userBId);
 
     void save(Match match);
+
+    List<UserId> findMatchedUserIdsByUserId(UserId userId);
 }
