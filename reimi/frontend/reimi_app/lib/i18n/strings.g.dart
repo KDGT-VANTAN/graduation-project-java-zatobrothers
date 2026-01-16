e5b4dcf21904905674d7567d58a139f838d9da84
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-16 11:06:06 +0900

    feat: 各画面のデータがない場合、nullの場合の多言語対応追加

--- reimi/frontend/reimi_app/lib/i18n/strings.g.dart
+++ reimi/frontend/reimi_app/lib/i18n/strings.g.dart
@@ -4,9 +4,9 @@
 /// To regenerate, run: `dart run slang`
 ///
 /// Locales: 2
-/// Strings: 1082 (541 per locale)
+/// Strings: 1106 (553 per locale)
 ///
-/// Built on 2026-01-13 at 10:41 UTC
+/// Built on 2026-01-16 at 01:52 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
