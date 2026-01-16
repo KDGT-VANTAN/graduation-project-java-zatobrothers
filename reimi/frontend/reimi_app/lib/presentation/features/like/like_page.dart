791896ea83b2c76ca7e977d19542f36a60d1e0fc
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-16 10:32:22 +0900

    feat: エラーが発生時にスナックバーを表示 +リフレッシュ機能追加 + null or loading時の表示追加 + 初期表示のアニメーション追加

--- reimi/frontend/reimi_app/lib/presentation/features/like/like_page.dart
+++ reimi/frontend/reimi_app/lib/presentation/features/like/like_page.dart
@@ -1,64 +1,130 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:reimi_app/presentation/features/like/notifiers/like_users_notifier.dart';
+import 'package:reimi_app/i18n/strings.g.dart';
+import 'package:reimi_app/presentation/features/like/notifiers/like_notifier.dart';
+import 'package:reimi_app/presentation/features/like/states/like_state.dart';
 import 'package:reimi_app/presentation/features/like/widgets/like_segment_switch.dart';
 import 'package:reimi_app/presentation/features/like/widgets/small_user_card.dart';
 import 'package:reimi_app/presentation/features/profile/pages/profile_detail_page.dart';
+import 'package:reimi_app/presentation/shared/widgets/app_snack_bar.dart';
 import 'package:reimi_app/presentation/shared/widgets/background_container_noon.dart';
+import 'package:reimi_app/presentation/shared/widgets/sliver_widgets.dart';
 
-class LikePage extends ConsumerWidget {
+class LikePage extends HookConsumerWidget {
   const LikePage({super.key});
   static String get routeName => 'like';
   static String get routeLocation => '/$routeName';
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final usersAsync = ref.watch(likeUsersNotifierProvider);
+    final theme = Theme.of(context);
+    final t = Translations.of(context);
+    final mainController = useAnimationController(
+      duration: const Duration(milliseconds: 800),
+    )..forward();
+
+    final notifier = ref.read(likeNotifierProvider.notifier);
+    final users =
+        ref.watch(likeNotifierProvider.select((state) => state.users));
+    final isLoading =
+        ref.watch(likeNotifierProvider.select((state) => state.isLoading));
+
+    useEffect(() {
+      Future.microtask(() {
+        notifier.init();
+      });
+
+      final subscription = ref.listenManual<LikeState>(
+        likeNotifierProvider,
+        (prev, next) {
+          if (next.errorMessage == null) return;
+          AppSnackBar.error(context, next.errorMessage!);
+        },
+      );
+
+      return subscription.close;
+    }, []);
+
     return Scaffold(
       body: BackgroundContainerNoon(
         child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18),
-            child: Column(
-              children: [
-                const SizedBox(height: 16),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 2),
-                  child: LikeSegmentSwitch(),
+          child: RefreshIndicator(
+            onRefresh: () async {
+              await notifier.refresh();
+            },
+            child: CustomScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              slivers: [
+                const Gap(height: 16),
+                const SliverPadding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  sliver: SliverToBoxAdapter(
+                    child: LikeSegmentSwitch(),
+                  ),
                 ),
-                const SizedBox(height: 16),
-                usersAsync.when(
-                  data: (users) {
-                    if (users.isEmpty) {
-                      return const Text('新しい出会いをみつけましょう！');
-                    }
-                    return Expanded(
-                      child: GridView.count(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 16,
-                        crossAxisSpacing: 16,
-                        childAspectRatio: 0.85,
-                        children: [
-                          ...users.map((user) {
-                            return SmallUserCard(
-                              user: user,
-                              onTap: () {
-                                context.push(
-                                  ProfileDetailPage.routeLocation,
-                                  extra: {'userId': user.id},
-                                );
-                              },
-                            );
-                          }),
-                        ],
+                const Gap(height: 16),
+                if (isLoading) ...[
+                  const SliverFillRemaining(
+                    hasScrollBody: false,
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  ),
+                ] else if (users.isEmpty) ...[
+                  SliverPadding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    sliver: SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: Center(
+                        child: Text(
+                          t.likePage.isEmptyCase,
+                          style: theme.textTheme.bodyMedium,
+                        ),
                       ),
-                    );
-                  },
-                  loading: () =>
-                      const Center(child: CircularProgressIndicator()),
-                  error: (e, _) => Center(child: Text('Error: $e')),
-                ),
+                    ),
+                  )
+                ] else ...[
+                  SliverPadding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    sliver: SliverGrid(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, index) {
+                          return ScaleTransition(
+                            scale: Tween(begin: 0.9, end: 1.0).animate(
+                              CurvedAnimation(
+                                parent: mainController,
+                                curve: Curves.easeOutBack,
+                              ),
+                            ),
+                            child: FadeTransition(
+                              opacity: mainController,
+                              child: SmallUserCard(
+                                user: users[index],
+                                onTap: () {
+                                  context.push(
+                                    ProfileDetailPage.routeLocation,
+                                    extra: {'userId': users[index].id},
+                                  );
+                                },
+                              ),
+                            ),
+                          );
+                        },
+                        childCount: users.length,
+                      ),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 16,
+                        mainAxisSpacing: 16,
+                        childAspectRatio: 0.85,
+                      ),
+                    ),
+                  ),
+                  const Gap(height: 24),
+                ],
               ],
             ),
           ),
