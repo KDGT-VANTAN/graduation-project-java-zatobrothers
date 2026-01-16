afb67c52d03582bcf6fb83910299a1cc4ab03238
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-16 10:46:05 +0900

    feat: セグメントの状態を管理するstate追加

--- reimi/frontend/reimi_app/lib/presentation/features/chat/states/chat_detail_state.dart
+++ reimi/frontend/reimi_app/lib/presentation/features/chat/states/chat_detail_state.dart
@@ -1,6 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:reimi_app/domain/read_models/chat_message_read_model.dart';
 import 'package:reimi_app/domain/read_models/user_with_profile_read_model.dart';
+import 'package:reimi_app/presentation/features/chat/enum/chat_segment.dart';
 
 part 'chat_detail_state.freezed.dart';
 
@@ -9,6 +10,7 @@ abstract class ChatDetailState with _$ChatDetailState {
   const factory ChatDetailState({
     @Default(<ChatMessageReadModel>[]) List<ChatMessageReadModel> chatMessages,
     UserWithProfileReadModel? userProfile,
+    @Default(ChatSegment.message) ChatSegment segment,
     @Default(false) bool isLoadingMessages,
     @Default(false) bool isLoadingProfile,
     String? errorMessage,
