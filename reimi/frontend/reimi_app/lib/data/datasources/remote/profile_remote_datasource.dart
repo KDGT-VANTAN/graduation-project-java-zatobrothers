b68c5532631dc6594221c3037a6547d8f2c134f6
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-16 10:20:20 +0900

    feat: プロフィールを取得・更新するapiを叩く処理を実装

--- reimi/frontend/reimi_app/lib/data/datasources/remote/profile_remote_datasource.dart
+++ reimi/frontend/reimi_app/lib/data/datasources/remote/profile_remote_datasource.dart
@@ -1,19 +1,50 @@
+import 'package:dio/dio.dart';
+import 'package:reimi_app/core/error/api_exception.dart';
 import 'package:reimi_app/data/dtos/update_profile_dto.dart';
+import 'package:reimi_app/data/extensions/update_profile_dto_extension.dart';
 import 'package:reimi_app/domain/read_models/user_with_profile_read_model.dart';
 
 abstract class ProfileRemoteDataSource {
-  Future<UserWithProfileReadModel?> fetchUserProfile(String userId);
-  Future<void> updateUserProfile(UpdateProfileDto dto);
+  Future<UserWithProfileReadModel> fetchUserProfile(String userId);
+  Future<void> updateUserProfile({
+    required UpdateProfileDto dto,
+    required String userId,
+  });
 }
 
 class ProfileRemoteDataSourceImpl implements ProfileRemoteDataSource {
-  const ProfileRemoteDataSourceImpl();
+  const ProfileRemoteDataSourceImpl(this._dio);
+  final Dio _dio;
 
   @override
-  Future<UserWithProfileReadModel?> fetchUserProfile(String userId) async {
-    return null;
+  Future<UserWithProfileReadModel> fetchUserProfile(String userId) async {
+    try {
+      final response = await _dio.get('/users/$userId/profile');
+      return UserWithProfileReadModel.fromJson(response.data);
+    } on DioException catch (e) {
+      throw ApiException.fromDioError(e);
+    }
   }
 
   @override
-  Future<void> updateUserProfile(UpdateProfileDto dto) async {}
+  Future<void> updateUserProfile({
+    required UpdateProfileDto dto,
+    required String userId,
+  }) async {
+    final formData = await dto.toFormData();
+    try {
+      final response = await _dio.put(
+        '/users/$userId/profile',
+        data: formData,
+        options: Options(
+          contentType: 'multipart/form-data',
+        ),
+      );
+      if (response.statusCode != 204) {
+        throw ApiException.fromResponse(response);
+      }
+    } on DioException catch (e) {
+      throw ApiException.fromDioError(e);
+    }
+  }
 }
