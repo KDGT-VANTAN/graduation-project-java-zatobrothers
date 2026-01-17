900a278f87c86411b0efb98fb5423e97b3a12604
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-18 01:22:38 +0900

    feat: メッセージリポジトリの実装クラスを作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/repository/message/MessageRepositoryImpl.java
@@ -0,0 +1,26 @@
+package com.reimi.reimi_app.infrastructure.persistence.repository.message;
+
+import org.springframework.stereotype.Repository;
+
+import com.reimi.reimi_app.domain.model.message.Message;
+import com.reimi.reimi_app.domain.repository.MessageRepository;
+import com.reimi.reimi_app.infrastructure.persistence.entity.MessageEntity;
+import com.reimi.reimi_app.infrastructure.persistence.mapper.MessageMapper;
+
+@Repository
+public class MessageRepositoryImpl implements MessageRepository {
+
+    private final JpaMessageRepository jpaMessageRepository;
+
+    private MessageRepositoryImpl(
+        JpaMessageRepository jpaMessageRepository
+    ) {
+        this.jpaMessageRepository = jpaMessageRepository;
+    }
+
+    @Override
+    public void save(Message message) {
+        MessageEntity messageEntity = MessageMapper.toTextEntity(message);
+        jpaMessageRepository.save(messageEntity);
+    }
+}
