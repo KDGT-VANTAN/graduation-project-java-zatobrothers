9e2c5f9a038b57df862449873bc2e909b1a33867
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-18 05:53:03 +0900

    feat: スキーマを定義

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/dto/request/SendMessageRequest.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/dto/request/SendMessageRequest.java
@@ -1,5 +1,12 @@
 package com.reimi.reimi_app.infrastructure.web.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
 public record SendMessageRequest(
+
+    @NotBlank(message = "メッセージは必須です")
+    @Schema(description = "テキストメッセージ", example = "よろしくお願いします！")
     String text
+
 ) {}
