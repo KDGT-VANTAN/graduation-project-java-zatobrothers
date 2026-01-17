80eaf696d5d175fd8c5fb7ed4bed581e033df670
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-18 02:14:47 +0900

    feat: マッチングリポジトリとチャットルームリポジトリのインターフェイスにメソッド追加

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/repository/ChatRoomRepository.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/repository/ChatRoomRepository.java
@@ -1,8 +1,13 @@
 package com.reimi.reimi_app.domain.repository;
 
+import java.util.Optional;
+
 import com.reimi.reimi_app.domain.model.chatroom.ChatRoom;
+import com.reimi.reimi_app.domain.model.chatroom.ChatRoomId;
 
 public interface ChatRoomRepository {
+
+    Optional<ChatRoom> findByChatRoomId(ChatRoomId chatRoomId);
 
     void save(ChatRoom chatRoom);
 }
