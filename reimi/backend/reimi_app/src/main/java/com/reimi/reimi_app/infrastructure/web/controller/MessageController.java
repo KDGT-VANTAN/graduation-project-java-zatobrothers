39e94df34541b47dc84b0930c6b2f5cc61dbf898
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-18 02:47:07 +0900

    feat: Controllerの作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/MessageController.java
@@ -0,0 +1,58 @@
+package com.reimi.reimi_app.infrastructure.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reimi.reimi_app.application.command.SendTextMessageCommand;
+import com.reimi.reimi_app.application.exception.client.ResourceNotFoundException;
+import com.reimi.reimi_app.application.usecase.MessageUseCase;
+import com.reimi.reimi_app.application.usecase.UserUseCase;
+import com.reimi.reimi_app.domain.model.chatroom.ChatRoomId;
+import com.reimi.reimi_app.domain.model.user.User;
+import com.reimi.reimi_app.infrastructure.web.dto.request.SendMessageRequest;
+import com.reimi.reimi_app.security.AuthenticatedUserProvider;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/chatRooms")
+public class MessageController {
+
+    private final MessageUseCase messageUseCase;
+    private final UserUseCase userUseCase;
+    private final AuthenticatedUserProvider authenticatedUserProvider;
+
+    public MessageController(
+        MessageUseCase messageUseCase,
+        UserUseCase userUseCase,
+        AuthenticatedUserProvider authenticatedUserProvider
+    ) {
+        this.messageUseCase = messageUseCase;
+        this.userUseCase = userUseCase;
+        this.authenticatedUserProvider = authenticatedUserProvider;
+    }
+
+    @PostMapping("/{chatRoomId}/messages")
+    public ResponseEntity<Void> sendTextMessage(
+        @PathVariable("chatRoomId") ChatRoomId chatRoomId,
+        @RequestBody SendMessageRequest request
+    ) {
+        String myFirebaseUid = authenticatedUserProvider.getFirebaseUid();
+        User user = userUseCase.getUser(myFirebaseUid)
+            .orElseThrow(() -> new ResourceNotFoundException("ユーザー"));
+
+        messageUseCase.sendText(
+            new SendTextMessageCommand(
+                chatRoomId,
+                user.getId(),
+                request.text()
+            )
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
