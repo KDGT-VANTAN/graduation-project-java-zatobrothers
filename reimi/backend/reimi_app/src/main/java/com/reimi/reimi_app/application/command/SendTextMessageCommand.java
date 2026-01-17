c056de6abf7afc2d3ef3a4240447c04623f43135
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-17 10:51:22 +0900

    feat: メッセージ送信用として分かりやすい命名にする

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/application/command/SendTextMessageCommand.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/application/command/SendTextMessageCommand.java
@@ -3,7 +3,7 @@ package com.reimi.reimi_app.application.command;
 import com.reimi.reimi_app.domain.model.chatroom.ChatRoomId;
 import com.reimi.reimi_app.domain.model.user.UserId;
 
-public record SendMessageCommand(
+public record SendTextMessageCommand(
     ChatRoomId chatRoomId,
     UserId senderId,
     String text
