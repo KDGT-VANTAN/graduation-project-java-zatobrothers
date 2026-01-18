f498ccd0b1038db59db8ddb3291e7f18632e2848
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-18 22:24:51 +0900

    feat: マッチング成立画面を状態管理するnotifierを実装

---
+++ reimi/frontend/reimi_app/lib/presentation/features/matching/notifiers/matching_notifier.dart
@@ -0,0 +1,37 @@
+import 'package:reimi_app/domain/read_models/matching_users_read_model.dart';
+import 'package:reimi_app/gen/assets.gen.dart';
+import 'package:reimi_app/presentation/features/matching/states/matching_state.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'matching_notifier.g.dart';
+
+@riverpod
+class MatchingNotifier extends _$MatchingNotifier {
+  @override
+  MatchingState build() {
+    return const MatchingState();
+  }
+
+  Future<void> init() async {
+    await loadMatchingUsers();
+  }
+
+  Future<void> loadMatchingUsers() async {
+    state = state.copyWith(isLoading: true, errorMessage: null);
+    try {
+      state = state.copyWith(
+        users: MatchingUsersReadModel(
+          otherUserName: 'さくら',
+          otherUserMainPhotoUrl: Assets.images.sample.user003SampleImage.path,
+          currentUserMainPhotoUrl: Assets.images.sample.user001SampleImage.path,
+        ),
+        isLoading: false,
+      );
+    } catch (e) {
+      state = state.copyWith(
+        isLoading: false,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+}
