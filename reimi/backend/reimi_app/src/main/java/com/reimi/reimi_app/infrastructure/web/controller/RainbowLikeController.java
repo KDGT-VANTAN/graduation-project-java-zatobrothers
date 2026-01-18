efde3fac4eff3d327a8f388c600ad308f18eb469
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-19 03:29:55 +0900

    feat: Controllerの作成とユーザーにレインボーいいねが送信できるAPIを定義

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/RainbowLikeController.java
@@ -0,0 +1,44 @@
+package com.reimi.reimi_app.infrastructure.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reimi.reimi_app.application.command.SendRainbowLikeCommand;
+import com.reimi.reimi_app.application.usecase.RainbowLikeUseCase;
+import com.reimi.reimi_app.domain.model.user.UserId;
+import com.reimi.reimi_app.infrastructure.web.dto.request.SendRainbowLikeRequest;
+import com.reimi.reimi_app.infrastructure.web.openapi.rainbowlike.RainbowLikeUserApi;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/rainbowLikes")
+public class RainbowLikeController {
+
+    private final RainbowLikeUseCase rainbowLikeUseCase;
+
+    public RainbowLikeController(
+        RainbowLikeUseCase rainbowLikeUseCase
+    ) {
+        this.rainbowLikeUseCase = rainbowLikeUseCase;
+    }
+
+    @PostMapping("/{userId}")
+    @RainbowLikeUserApi
+    public ResponseEntity<Void> RainbowLike(
+        @PathVariable("userId") UserId toUserId,
+        @RequestBody SendRainbowLikeRequest request) {
+        rainbowLikeUseCase.rainbowLikeUser(
+            new SendRainbowLikeCommand(
+                toUserId,
+                request.message()
+            )
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
