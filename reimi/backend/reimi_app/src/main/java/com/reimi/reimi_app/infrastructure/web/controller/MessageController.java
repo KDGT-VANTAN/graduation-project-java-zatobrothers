d143bf2cf97ae732b205f059f3a3959c0be75b12
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-18 22:16:43 +0900

    feat: チャットルームのメッセージ一覧取得APIを定義

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/MessageController.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/MessageController.java
@@ -1,7 +1,10 @@
 package com.reimi.reimi_app.infrastructure.web.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,8 @@ import com.reimi.reimi_app.application.usecase.UserUseCase;
 import com.reimi.reimi_app.domain.model.chatroom.ChatRoomId;
 import com.reimi.reimi_app.domain.model.user.User;
 import com.reimi.reimi_app.infrastructure.web.dto.request.SendMessageRequest;
+import com.reimi.reimi_app.infrastructure.web.dto.response.GetMessageListResponse;
+import com.reimi.reimi_app.infrastructure.web.openapi.message.GetChatRoomMessagesApi;
 import com.reimi.reimi_app.infrastructure.web.openapi.message.SendTextMessageApi;
 import com.reimi.reimi_app.security.AuthenticatedUserProvider;
 
@@ -57,5 +62,16 @@ public class MessageController {
         );
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{chatRoomId}/messages")
+    @GetChatRoomMessagesApi
+    public ResponseEntity<List<GetMessageListResponse>> getChatRoomMessages(@PathVariable ChatRoomId chatRoomId) {
+        List<GetMessageListResponse> response = messageUseCase.getChatRoomMessageList(chatRoomId)
+        .stream()
+        .map(GetMessageListResponse::from)
+        .toList();
+
+        return ResponseEntity.ok(response);
     }
 }
