a6f055efc745916353437162f81e65c2acfa0905
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-19 02:56:54 +0900

    feat: すでにレインボーいいねが送られていた場合のカスタム例外処理を作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/application/exception/client/RainbowLikeAlreadyExistsException.java
@@ -0,0 +1,15 @@
+package com.reimi.reimi_app.application.exception.client;
+
+import org.springframework.http.HttpStatus;
+
+import com.reimi.reimi_app.application.exception.ClientErrorException;
+
+public class RainbowLikeAlreadyExistsException extends ClientErrorException {
+    public RainbowLikeAlreadyExistsException() {
+        super(
+            "RAINBOW_LIKE_ALREADY_SENT",
+            "既にレインボーいいねが送られています",
+            HttpStatus.CONFLICT
+        );
+    }
+}
