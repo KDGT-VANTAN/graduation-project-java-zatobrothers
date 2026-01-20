ae87c5a4cf2874dc7aeb531c7a11b704c6c2da65
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-20 19:05:48 +0900

    feat: ロケール設定のデータ操作を状態管理するプロバイダー実装

---
+++ reimi/frontend/reimi_app/lib/core/locale/locale_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'locale_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$localeStorageHash() => r'f298f4835aa0bb51798797f525002f3a653b2825';
+
+/// See also [localeStorage].
+@ProviderFor(localeStorage)
+final localeStorageProvider = AutoDisposeProvider<LocaleStorage>.internal(
+  localeStorage,
+  name: r'localeStorageProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$localeStorageHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef LocaleStorageRef = AutoDisposeProviderRef<LocaleStorage>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
