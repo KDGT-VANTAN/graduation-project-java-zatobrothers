74d96152e850f109a2f99a100fd193341871a655
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-20 19:03:46 +0900

    feat: 言語設定を状態管理するプロバイダー実装

---
+++ reimi/frontend/reimi_app/lib/core/locale/locale_notifier.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'locale_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$localeNotifierHash() => r'aeeebc3726ffdf43ecca6b00350ee0b4fecb6b8e';
+
+/// See also [LocaleNotifier].
+@ProviderFor(LocaleNotifier)
+final localeNotifierProvider =
+    AutoDisposeNotifierProvider<LocaleNotifier, AppLocale>.internal(
+  LocaleNotifier.new,
+  name: r'localeNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$localeNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LocaleNotifier = AutoDisposeNotifier<AppLocale>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
