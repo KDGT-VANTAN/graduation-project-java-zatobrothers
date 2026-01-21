3d3a57d31c84cd1b8dfb0de3aadc85680e8e1fe7
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-21 17:56:32 +0900

    feat: 16タイプのコードをenum（列挙型）で定義

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/model/weatherpersonality/WeatherPersonalityCode.java
@@ -0,0 +1,24 @@
+package com.reimi.reimi_app.domain.model.weatherpersonality;
+
+public enum WeatherPersonalityCode {
+
+    SPOE,
+    SPOR,
+    SPIE,
+    SPIR,
+
+    SFOE,
+    SFOR,
+    SFIE,
+    SFIR,
+
+    NPOE,
+    NPOR,
+    NPIE,
+    NPIR,
+
+    NFOE,
+    NFOR,
+    NFIE,
+    NFIR;
+}
