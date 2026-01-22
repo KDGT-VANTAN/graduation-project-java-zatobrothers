2bdb9050493271a2bda8ea4edcd1cf6822101c10
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-23 01:15:48 +0900

    feat: マイウェザーリポート画面を状態管理するnotifier実装

---
+++ reimi/frontend/reimi_app/lib/presentation/features/weather_report/notifiers/my_weather_report_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'my_weather_report_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$myWeatherReportNotifierHash() =>
+    r'0c53c0cac0046de66a2057fdaa817606fa14ca63';
+
+/// See also [MyWeatherReportNotifier].
+@ProviderFor(MyWeatherReportNotifier)
+final myWeatherReportNotifierProvider = AutoDisposeNotifierProvider<
+    MyWeatherReportNotifier, MyWeatherReportState>.internal(
+  MyWeatherReportNotifier.new,
+  name: r'myWeatherReportNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$myWeatherReportNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MyWeatherReportNotifier = AutoDisposeNotifier<MyWeatherReportState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
