b3a62d583eaae19ffa11cd8bb17128c1d0c5b98b
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-23 23:59:11 +0900

    feat: ウェザーパーソナリティのユースケースを実装

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/service/WeatherPersonalityUseCaseImpl.java
@@ -0,0 +1,43 @@
+package com.reimi.reimi_app.infrastructure.service;
+
+import org.springframework.stereotype.Service;
+
+import com.reimi.reimi_app.application.command.DiagnoseWeatherPersonalityCommand;
+import com.reimi.reimi_app.application.usecase.WeatherPersonalityUseCase;
+import com.reimi.reimi_app.domain.model.weatherpersonality.UserWeatherPersonalityType;
+import com.reimi.reimi_app.domain.model.weatherpersonality.WeatherPersonalityCode;
+import com.reimi.reimi_app.domain.model.weatherpersonality.WeatherPersonalityDiagnosis;
+import com.reimi.reimi_app.domain.model.weatherpersonality.WeatherPersonalityScore;
+import com.reimi.reimi_app.domain.repository.UserWeatherPersonalityTypeRepository;
+
+@Service
+public class WeatherPersonalityUseCaseImpl implements WeatherPersonalityUseCase {
+
+    private final UserWeatherPersonalityTypeRepository userWeatherPersonalityTypeRepository;
+
+    public WeatherPersonalityUseCaseImpl(
+        UserWeatherPersonalityTypeRepository userWeatherPersonalityTypeRepository
+    ) {
+        this.userWeatherPersonalityTypeRepository = userWeatherPersonalityTypeRepository;
+    }
+
+    @Override
+    public UserWeatherPersonalityType diagnose(DiagnoseWeatherPersonalityCommand command) {
+
+        WeatherPersonalityDiagnosis diagnosis = new WeatherPersonalityDiagnosis();
+
+        WeatherPersonalityScore weatherPersonalityScore = diagnosis.diagnoseScore(command.answers());
+
+        WeatherPersonalityCode code = diagnosis.decideType(weatherPersonalityScore, command.answers());
+
+        UserWeatherPersonalityType result = UserWeatherPersonalityType.result(
+            command.userId(),
+            code,
+            weatherPersonalityScore
+        );
+
+        userWeatherPersonalityTypeRepository.save(result);
+
+        return result;
+    }
+}
