4a6f8703744bb6540cb70a4313a70b99b9cb5097
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-24 04:13:29 +0900

    feat: Controllerの作成＆ウェザーパーソナリティ診断結果スコアリングのPOST APIメソッドを定義

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/v1/WeatherPersonalityController.java
@@ -0,0 +1,84 @@
+package com.reimi.reimi_app.infrastructure.web.controller.v1;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reimi.reimi_app.application.command.DiagnoseWeatherPersonalityCommand;
+import com.reimi.reimi_app.application.exception.client.ResourceNotFoundException;
+import com.reimi.reimi_app.application.usecase.UserUseCase;
+import com.reimi.reimi_app.application.usecase.WeatherPersonalityUseCase;
+import com.reimi.reimi_app.domain.model.user.User;
+import com.reimi.reimi_app.domain.model.weatherpersonality.AnswerChoice;
+import com.reimi.reimi_app.infrastructure.web.dto.request.DiagnoseWeatherPersonalityRequest;
+import com.reimi.reimi_app.infrastructure.web.dto.response.DiagnoseResultWeatherPersonalityResponse;
+import com.reimi.reimi_app.security.AuthenticatedUserProvider;
+
+
+@RestController
+public class WeatherPersonalityController extends ApiV1Controller {
+
+    private final WeatherPersonalityUseCase weatherPersonalityUseCase;
+    private final AuthenticatedUserProvider authenticatedUserProvider;
+    private final UserUseCase userUseCase;
+
+
+    public WeatherPersonalityController(
+        WeatherPersonalityUseCase weatherPersonalityUseCase,
+        AuthenticatedUserProvider authenticatedUserProvider,
+        UserUseCase userUseCase
+    ) {
+        this.weatherPersonalityUseCase = weatherPersonalityUseCase;
+        this.authenticatedUserProvider = authenticatedUserProvider;
+        this.userUseCase = userUseCase;
+    }
+
+    @PostMapping("/diagnoses/score/user-weather-personality-type")
+    public ResponseEntity<DiagnoseResultWeatherPersonalityResponse> diagnose(
+        @RequestBody DiagnoseWeatherPersonalityRequest request
+    ) {
+        List<AnswerChoice> answers = List.of(
+            request.q1Answer(),
+            request.q2Answer(),
+            request.q3Answer(),
+            request.q4Answer(),
+            request.q5Answer(),
+            request.q6Answer(),
+            request.q7Answer(),
+            request.q8Answer(),
+            request.q9Answer(),
+            request.q10Answer(),
+            request.q11Answer(),
+            request.q12Answer(),
+            request.q13Answer(),
+            request.q14Answer(),
+            request.q15Answer(),
+            request.q16Answer()
+        );
+
+        String myFirebaseUid = authenticatedUserProvider.getFirebaseUid();
+        User user = userUseCase.getUser(myFirebaseUid)
+            .orElseThrow(() -> new ResourceNotFoundException("ユーザー"));
+
+        var result = weatherPersonalityUseCase.diagnose(
+            new DiagnoseWeatherPersonalityCommand(
+                user.getId(),
+                answers
+            )
+        );
+
+        DiagnoseResultWeatherPersonalityResponse response =
+            new DiagnoseResultWeatherPersonalityResponse(
+                result.getWeatherPersonalityType().getCode(),
+                result.getWeatherPersonalityType().getName(),
+                result.getWeatherPersonalityType().getDescription(),
+                result.getWeatherPersonalityType().getImagePath()
+            );
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
