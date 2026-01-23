03241091c45a9ede982244e38c983f26bdcdbc6e
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-23 12:33:23 +0900

    feat: コードからエンティティを取得してくるメソッドを追加

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/repository/weatherpersonalitytype/JpaWeatherPersonalityTypeRepository.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/repository/weatherpersonalitytype/JpaWeatherPersonalityTypeRepository.java
@@ -1,8 +1,11 @@
 package com.reimi.reimi_app.infrastructure.persistence.repository.weatherpersonalitytype;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.reimi.reimi_app.infrastructure.persistence.entity.WeatherPersonalityTypeEntity;
 
 public interface JpaWeatherPersonalityTypeRepository extends JpaRepository<WeatherPersonalityTypeEntity, String> {
+    Optional<WeatherPersonalityTypeEntity> findByCode(String code);
 }
