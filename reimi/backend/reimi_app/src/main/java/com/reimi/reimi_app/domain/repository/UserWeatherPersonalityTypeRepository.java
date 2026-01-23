e60e1dbbb1bf3ff6a94a0ef4cf0cfdb7a4a53849
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-23 23:52:57 +0900

    feat: ユーザーウェザーパーソナリティタイプリポジトリのインターフェイスを作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/repository/UserWeatherPersonalityTypeRepository.java
@@ -0,0 +1,7 @@
+package com.reimi.reimi_app.domain.repository;
+
+import com.reimi.reimi_app.domain.model.weatherpersonality.UserWeatherPersonalityType;
+
+public interface UserWeatherPersonalityTypeRepository {
+    void save(UserWeatherPersonalityType userWeatherPersonalityType);
+}
