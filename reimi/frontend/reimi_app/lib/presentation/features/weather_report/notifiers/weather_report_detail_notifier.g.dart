0221505f5b2b1c5cd6839295ca4d6f511a2ca880
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-24 23:53:33 +0900

    fix: 細かい修正

--- reimi/frontend/reimi_app/lib/presentation/features/weather_report/notifiers/weather_report_detail_notifier.g.dart
+++ reimi/frontend/reimi_app/lib/presentation/features/weather_report/notifiers/weather_report_detail_notifier.g.dart
@@ -7,7 +7,7 @@ part of 'weather_report_detail_notifier.dart';
 // **************************************************************************
 
 String _$weatherReportDetailNotifierHash() =>
-    r'a1b575f5a452d5c3a05f4778827457da7896e17b';
+    r'4b9a9609f4f40802ccc4a649f2c23e0884f0c965';
 
 /// See also [WeatherReportDetailNotifier].
 @ProviderFor(WeatherReportDetailNotifier)
