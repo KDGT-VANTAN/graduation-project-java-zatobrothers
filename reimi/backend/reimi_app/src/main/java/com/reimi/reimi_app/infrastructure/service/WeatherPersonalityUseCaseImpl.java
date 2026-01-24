9ab903ecc23c0f410accfe314f452236ce4e7180
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-24 12:35:02 +0900

    feat: ユーザーの診断結果が存在するか確認する処理を追加

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/service/WeatherPersonalityUseCaseImpl.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/service/WeatherPersonalityUseCaseImpl.java
@@ -3,6 +3,7 @@ package com.reimi.reimi_app.infrastructure.service;
 import org.springframework.stereotype.Service;
 
 import com.reimi.reimi_app.application.command.DiagnoseWeatherPersonalityCommand;
+import com.reimi.reimi_app.application.exception.client.DiagnoseResultAlreadyExistsException;
 import com.reimi.reimi_app.application.usecase.WeatherPersonalityUseCase;
 import com.reimi.reimi_app.domain.model.weatherpersonality.UserWeatherPersonalityType;
 import com.reimi.reimi_app.domain.model.weatherpersonality.WeatherPersonalityCode;
@@ -25,10 +26,18 @@ public class WeatherPersonalityUseCaseImpl implements WeatherPersonalityUseCase 
     @Override
     public UserWeatherPersonalityType diagnose(DiagnoseWeatherPersonalityCommand command) {
 
+        // 既にユーザーが診断済みの場合は重複エラーとする
+        if (userWeatherPersonalityTypeRepository.exists(command.userId())) {
+            throw new DiagnoseResultAlreadyExistsException();
+        }
+
+        // 診断ロジックがあるドメインサービスをインスタンス化して使う
         WeatherPersonalityDiagnosis diagnosis = new WeatherPersonalityDiagnosis();
 
+        // 4つの軸ごとにスコアリングする
         WeatherPersonalityScore weatherPersonalityScore = diagnosis.diagnoseScore(command.answers());
 
+        //スコアリングした結果からタイプコードを決定する
         WeatherPersonalityCode code = diagnosis.decideType(weatherPersonalityScore, command.answers());
 
         WeatherPersonalityType type = WeatherPersonalityType.from(code);
