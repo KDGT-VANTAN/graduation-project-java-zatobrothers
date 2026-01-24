405205f24395a02ec14484d2ee333e401efc2a03
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-24 23:24:33 +0900

    feat: ウェザーパーソナリティ詳細取得のReadモデル実装

---
+++ reimi/frontend/reimi_app/lib/domain/read_models/weather_personality_detail_read_model.g.dart
@@ -0,0 +1,51 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather_personality_detail_read_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_WeatherPersonalityDetailReadModel _$WeatherPersonalityDetailReadModelFromJson(
+        Map<String, dynamic> json) =>
+    _WeatherPersonalityDetailReadModel(
+      typeCode: json['typeCode'] as String,
+      typeName: json['typeName'] as String,
+      typeCatchphrase: json['typeCatchphrase'] as String,
+      typeCharacterImageUrl: json['typeCharacterImageUrl'] as String,
+      rulingStatement: json['rulingStatement'] as String,
+      axisFeatures: (json['axisFeatures'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
+      axisScore: (json['axisScore'] as List<dynamic>)
+          .map((e) => (e as num).toInt())
+          .toList(),
+      behaviorTendencyList: (json['behaviorTendencyList'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
+      compatibleTypes: (json['compatibleTypes'] as List<dynamic>)
+          .map((e) =>
+              TypeCompatibilityReadModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      incompatibleTypes: (json['incompatibleTypes'] as List<dynamic>)
+          .map((e) =>
+              TypeCompatibilityReadModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      godsMessage: json['godsMessage'] as String,
+    );
+
+Map<String, dynamic> _$WeatherPersonalityDetailReadModelToJson(
+        _WeatherPersonalityDetailReadModel instance) =>
+    <String, dynamic>{
+      'typeCode': instance.typeCode,
+      'typeName': instance.typeName,
+      'typeCatchphrase': instance.typeCatchphrase,
+      'typeCharacterImageUrl': instance.typeCharacterImageUrl,
+      'rulingStatement': instance.rulingStatement,
+      'axisFeatures': instance.axisFeatures,
+      'axisScore': instance.axisScore,
+      'behaviorTendencyList': instance.behaviorTendencyList,
+      'compatibleTypes': instance.compatibleTypes,
+      'incompatibleTypes': instance.incompatibleTypes,
+      'godsMessage': instance.godsMessage,
+    };
