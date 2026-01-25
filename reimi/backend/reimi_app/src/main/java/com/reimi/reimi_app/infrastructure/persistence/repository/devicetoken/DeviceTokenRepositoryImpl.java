12e37a70ab7d4b102eedbf716ecc4783c957c0dc
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-25 16:28:02 +0900

    feat: デバイストークンのリポジトリ実装クラスを作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/repository/devicetoken/DeviceTokenRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.reimi.reimi_app.infrastructure.persistence.repository.devicetoken;
+
+import org.springframework.stereotype.Repository;
+
+import com.reimi.reimi_app.infrastructure.persistence.entity.DeviceTokenEntity;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class DeviceTokenRepositoryImpl {
+
+    private final JpaDeviceTokenRepository jpaDeviceTokenRepository;
+
+    public DeviceTokenRepositoryImpl(
+        JpaDeviceTokenRepository jpaDeviceTokenRepository
+    ) {
+        this.jpaDeviceTokenRepository = jpaDeviceTokenRepository;
+    }
+
+    public Optional<DeviceTokenEntity> findByToken(String token) {
+        return jpaDeviceTokenRepository.findByToken(token);
+    }
+
+    public List<DeviceTokenEntity> findByUserId(UUID userId) {
+        return jpaDeviceTokenRepository.findByUserId(userId);
+    }
+
+    public void save(DeviceTokenEntity entity) {
+        jpaDeviceTokenRepository.save(entity);
+    }
+
+}
