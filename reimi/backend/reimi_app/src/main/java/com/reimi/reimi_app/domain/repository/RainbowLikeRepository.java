38580c10e42677ab540e11beb8dc517052328f34
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-27 09:54:39 +0900

    feat: ログインユーザーがされたレインボーいいね一覧を取得メソッドを追加

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/repository/RainbowLikeRepository.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/repository/RainbowLikeRepository.java
@@ -1,5 +1,7 @@
 package com.reimi.reimi_app.domain.repository;
 
+import java.util.List;
+
 import com.reimi.reimi_app.domain.model.rainbowlike.RainbowLike;
 import com.reimi.reimi_app.domain.model.user.UserId;
 
@@ -8,4 +10,6 @@ public interface RainbowLikeRepository {
     boolean exists(UserId fromUserId, UserId toUserId);
 
     void save(RainbowLike rainbowLike);
+
+    List<RainbowLike> findRainbowLikesReceivedByToUserId(UserId toUserId);
 }
