8e067e32aea9de8c293fbac9a1ad227b60095331
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-27 19:53:13 +0900

    feat: Controllerに自分がレインボーいいねされたユーザー一覧取得のGETAPIを定義

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/v1/RainbowLikeController.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/v1/RainbowLikeController.java
@@ -1,7 +1,10 @@
 package com.reimi.reimi_app.infrastructure.web.controller.v1;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +13,7 @@ import com.reimi.reimi_app.application.command.SendRainbowLikeCommand;
 import com.reimi.reimi_app.application.usecase.RainbowLikeUseCase;
 import com.reimi.reimi_app.domain.model.user.UserId;
 import com.reimi.reimi_app.infrastructure.web.dto.request.SendRainbowLikeRequest;
+import com.reimi.reimi_app.infrastructure.web.dto.response.GetRainbowLikedUserListResponse;
 import com.reimi.reimi_app.infrastructure.web.openapi.rainbowlike.RainbowLikeUserApi;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -32,7 +36,7 @@ public class RainbowLikeController extends ApiV1Controller {
 
     @PostMapping(path = "/rainbow-likes/{userId}")
     @RainbowLikeUserApi
-    public ResponseEntity<Void> RainbowLike(
+    public ResponseEntity<Void> rainbowLike(
         @PathVariable("userId") UserId toUserId,
         @Valid @RequestBody SendRainbowLikeRequest request
     ) {
@@ -44,5 +48,23 @@ public class RainbowLikeController extends ApiV1Controller {
         );
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping(path = "/rainbow-likes/users/received")
+    public ResponseEntity<List<GetRainbowLikedUserListResponse>> getRainbowLikeReceivedUsers() {
+
+        List<GetRainbowLikedUserListResponse> response = rainbowLikeUseCase.getRainbowLikeReceivedUserList()
+                .stream()
+                .map(user -> new GetRainbowLikedUserListResponse(
+                    user.getId().value(),
+                    user.getName(),
+                    user.getBirthDate(),
+                    user.getAddress(),
+                    user.getSignedMainPhotoUrl(),
+                    user.getRainbowLikeMessage()
+                ))
+                .toList();
+
+        return ResponseEntity.ok(response);
     }
 }
