ffc3866d399a7799327a93fd0f6eb66986e8677a
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-27 10:05:24 +0900

    feat: ドメインモデルへの変換メソッド作成

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/mapper/RainbowLikeMapper.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/mapper/RainbowLikeMapper.java
@@ -1,11 +1,21 @@
 package com.reimi.reimi_app.infrastructure.persistence.mapper;
 
 import com.reimi.reimi_app.domain.model.rainbowlike.RainbowLike;
+import com.reimi.reimi_app.domain.model.rainbowlike.RainbowLikeId;
+import com.reimi.reimi_app.domain.model.user.UserId;
 import com.reimi.reimi_app.infrastructure.persistence.entity.RainbowLikeEntity;
 
 public class RainbowLikeMapper {
 
-        public static RainbowLikeEntity toEntity(RainbowLike rainbowLike) {
+    public static RainbowLike toDomain(RainbowLikeEntity entity) {
+        return RainbowLike.reconstruct(
+                new RainbowLikeId(entity.getId()),
+                new UserId(entity.getFromUserId()),
+                new UserId(entity.getToUserId()),
+                entity.getMessage()
+        );
+    }
+    public static RainbowLikeEntity toEntity(RainbowLike rainbowLike) {
         RainbowLikeEntity entity = new RainbowLikeEntity();
         entity.setId(rainbowLike.getId().value());
         entity.setFromUserId(rainbowLike.getFromUserId().value());
