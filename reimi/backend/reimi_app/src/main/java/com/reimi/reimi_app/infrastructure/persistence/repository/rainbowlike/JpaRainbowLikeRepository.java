232874b0750027119185bf67eb0892e9d6fe59e0
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-27 09:55:41 +0900

    feat: JPQLを用いてレインボーライクJPA Repositoryメソッド作成

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/repository/rainbowlike/JpaRainbowLikeRepository.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/repository/rainbowlike/JpaRainbowLikeRepository.java
@@ -1,11 +1,20 @@
 package com.reimi.reimi_app.infrastructure.persistence.repository.rainbowlike;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.reimi.reimi_app.infrastructure.persistence.entity.RainbowLikeEntity;
 
 public interface JpaRainbowLikeRepository extends JpaRepository<RainbowLikeEntity, UUID> {
     boolean existsByFromUserIdAndToUserId(UUID fromUserId, UUID toUserId);
+
+    @Query("""
+        select r
+        from RainbowLikeEntity r
+        where r.toUserId = :toUserId
+    """)
+    List<RainbowLikeEntity> findRainbowLikesReceivedByToUserId(UUID toUserId);
 }
