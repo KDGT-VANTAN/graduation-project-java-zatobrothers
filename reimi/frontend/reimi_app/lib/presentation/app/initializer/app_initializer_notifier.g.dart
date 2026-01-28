97fc2fa0b3bafe062053156f8046955a03fb4834
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-28 23:14:27 +0900

    feat: アプリ初期化のnotifier実装。通知の権限リクエストする処理とプッシュ通知に必要なデバイストークンを登録する処理を実装

---
+++ reimi/frontend/reimi_app/lib/presentation/app/initializer/app_initializer_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_initializer_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appInitializerNotifierHash() =>
+    r'263fdf56d3deb94bb8ae441077e5a182cb900890';
+
+/// See also [AppInitializerNotifier].
+@ProviderFor(AppInitializerNotifier)
+final appInitializerNotifierProvider =
+    AutoDisposeAsyncNotifierProvider<AppInitializerNotifier, void>.internal(
+  AppInitializerNotifier.new,
+  name: r'appInitializerNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$appInitializerNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AppInitializerNotifier = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
