777a4a25e16a6989742f9d037bb6249548636702
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-28 23:30:37 +0900

    feat: 外行動性→活動性に変更

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/model/weatherpersonality/WeatherPersonalityAxis.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/model/weatherpersonality/WeatherPersonalityAxis.java
@@ -3,7 +3,7 @@ package com.reimi.reimi_app.domain.model.weatherpersonality;
 public enum WeatherPersonalityAxis {
     SENSITIVITY("感受性"),
     PREPAREDNESS("準備性"),
-    ACTIVITY("外行動性"),
+    ACTIVITY("活動性"),
     MOTIVATION("動機特性");
 
     private final String label;
