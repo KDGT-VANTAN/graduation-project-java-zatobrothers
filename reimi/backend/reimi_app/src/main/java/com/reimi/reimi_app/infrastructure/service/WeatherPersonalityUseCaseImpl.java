2e507597f521dca9627da976b7eb5469226275cb
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-29 14:46:45 +0900

    feat: ウェザーパーソナリティの実装ユースケースを変更

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/service/WeatherPersonalityUseCaseImpl.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/service/WeatherPersonalityUseCaseImpl.java
@@ -4,31 +4,41 @@ import org.springframework.stereotype.Service;
 
 import com.reimi.reimi_app.application.command.DiagnoseWeatherPersonalityCommand;
 import com.reimi.reimi_app.application.exception.client.DiagnoseResultAlreadyExistsException;
+import com.reimi.reimi_app.application.exception.client.ResourceNotFoundException;
 import com.reimi.reimi_app.application.service.ImageUrlResolver;
 import com.reimi.reimi_app.application.usecase.WeatherPersonalityUseCase;
+import com.reimi.reimi_app.domain.model.user.User;
 import com.reimi.reimi_app.domain.model.weatherpersonality.UserWeatherPersonalityType;
 import com.reimi.reimi_app.domain.model.weatherpersonality.WeatherPersonalityCode;
 import com.reimi.reimi_app.domain.model.weatherpersonality.WeatherPersonalityDiagnosis;
 import com.reimi.reimi_app.domain.model.weatherpersonality.WeatherPersonalityScore;
 import com.reimi.reimi_app.domain.model.weatherpersonality.WeatherPersonalityType;
+import com.reimi.reimi_app.domain.repository.UserRepository;
 import com.reimi.reimi_app.domain.repository.UserWeatherPersonalityTypeRepository;
+import com.reimi.reimi_app.security.AuthenticatedUserProvider;
 
 @Service
 public class WeatherPersonalityUseCaseImpl implements WeatherPersonalityUseCase {
 
     private final UserWeatherPersonalityTypeRepository userWeatherPersonalityTypeRepository;
     private final ImageUrlResolver imageUrlResolver;
+    private final UserRepository userRepository;
+    private final AuthenticatedUserProvider authenticatedUserProvider;
 
     public WeatherPersonalityUseCaseImpl(
         UserWeatherPersonalityTypeRepository userWeatherPersonalityTypeRepository,
-        ImageUrlResolver imageUrlResolver
+        ImageUrlResolver imageUrlResolver,
+        UserRepository userRepository,
+        AuthenticatedUserProvider authenticatedUserProvider
     ) {
         this.userWeatherPersonalityTypeRepository = userWeatherPersonalityTypeRepository;
         this.imageUrlResolver = imageUrlResolver;
+        this.userRepository = userRepository;
+        this.authenticatedUserProvider = authenticatedUserProvider;
     }
 
     @Override
-    public UserWeatherPersonalityType diagnose(DiagnoseWeatherPersonalityCommand command) {
+    public void diagnose(DiagnoseWeatherPersonalityCommand command) {
 
         // 既にユーザーが診断済みの場合は重複エラーとする
         if (userWeatherPersonalityTypeRepository.exists(command.userId())) {
@@ -53,8 +63,20 @@ public class WeatherPersonalityUseCaseImpl implements WeatherPersonalityUseCase 
         );
 
         userWeatherPersonalityTypeRepository.save(result);
+    }
 
-        String typeImageUrl = imageUrlResolver.resolve(
+    @Override
+    public UserWeatherPersonalityType getUserResult() {
+
+    String firebaseUid = authenticatedUserProvider.getFirebaseUid();
+
+    User user = userRepository.findMeByFirebaseUid(firebaseUid)
+            .orElseThrow(() -> new ResourceNotFoundException("ユーザー"));
+
+    UserWeatherPersonalityType result = userWeatherPersonalityTypeRepository.findByUserId(user.getId())
+        .orElseThrow(() -> new ResourceNotFoundException("ウェザーパーソナリティ診断結果"));
+
+    String typeImageUrl = imageUrlResolver.resolve(
             result.getWeatherPersonalityType().getImagePath()
         );
 
