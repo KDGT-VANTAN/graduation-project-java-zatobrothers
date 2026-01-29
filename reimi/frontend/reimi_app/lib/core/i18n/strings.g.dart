fdb3b8719d18b37c28374e5e042d5e7d86db26ce
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-29 09:50:53 +0900

    fix: 自動ファイル生成による差分

--- reimi/frontend/reimi_app/lib/core/i18n/strings.g.dart
+++ reimi/frontend/reimi_app/lib/core/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 1200 (600 per locale)
 ///
-/// Built on 2026-01-28 at 09:23 UTC
+/// Built on 2026-01-28 at 16:52 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
