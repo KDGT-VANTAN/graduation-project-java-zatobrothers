aca7cab2eed4214de99429d464c195c0f42c2d33
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-01-30 19:13:53 +0900

    feat: ルートパスへのアクセスを許可

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/config/SecurityConfig.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
             .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(
+                    "/",
                     "/swagger-ui/**",
                     "/v3/api-docs/**",
                     "/images/weather-personalities/**"
