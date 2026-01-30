2ca5c65786932cc806bd429238f0fcec09205045
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-01-30 17:24:33 +0900

    feat: ウェザーパーソナリティ診断のapi、診断結果取得のapiをたたく処理を実装

--- reimi/frontend/reimi_app/lib/data/datasources/remote/weather_personality_remote_datasource.dart
+++ reimi/frontend/reimi_app/lib/data/datasources/remote/weather_personality_remote_datasource.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+import 'package:reimi_app/core/error/api_exception.dart';
 import 'package:reimi_app/data/dtos/test_weather_personality_dto.dart';
 import 'package:reimi_app/data/dtos/weather_personality_detail_dto.dart';
 import 'package:reimi_app/data/dtos/weather_personality_result_dto.dart';
@@ -12,18 +14,35 @@ abstract class WeatherPersonalityRemoteDataSource {
 
 class WeatherPersonalityRemoteDataSourceImpl
     implements WeatherPersonalityRemoteDataSource {
-  const WeatherPersonalityRemoteDataSourceImpl();
+  const WeatherPersonalityRemoteDataSourceImpl(this._dio);
+  final Dio _dio;
 
   @override
-  Future<void> testWeatherPersonality(TestWeatherPersonalityDto dto) {
-    // TODO: implement postWeatherReport
-    throw UnimplementedError();
+  Future<void> testWeatherPersonality(TestWeatherPersonalityDto dto) async {
+    try {
+      final response = await _dio.post(
+        '/api/v1/user-weather-personality-type',
+        data: dto.toJson(),
+        options: Options(
+          validateStatus: (_) => true,
+        ),
+      );
+      if (response.statusCode != 201) {
+        throw ApiException.fromResponse(response);
+      }
+    } on DioException catch (e) {
+      throw ApiException.fromDioError(e);
+    }
   }
 
   @override
-  Future<WeatherPersonalityResultDto> fetchWeatherPersonalityResult() {
-    // TODO: implement fetchWeatherPersonalityResult
-    throw UnimplementedError();
+  Future<WeatherPersonalityResultDto> fetchWeatherPersonalityResult() async {
+    try {
+      final response = await _dio.get('/api/v1/user-weather-personality-type');
+      return WeatherPersonalityResultDto.fromJson(response.data);
+    } on DioException catch (e) {
+      throw ApiException.fromDioError(e);
+    }
   }
 
   @override
