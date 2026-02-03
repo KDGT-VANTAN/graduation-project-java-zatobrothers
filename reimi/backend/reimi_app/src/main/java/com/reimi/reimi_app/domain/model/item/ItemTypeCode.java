bdc37201525d066ca5b90c8b7492df9c8a3ea6e7
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-02-03 19:37:01 +0900

    feat: アイテムのコードをenum（列挙型）で定義

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/model/item/ItemTypeCode.java
@@ -0,0 +1,23 @@
+package com.reimi.reimi_app.domain.model.item;
+
+public enum ItemTypeCode {
+    RAINBOW_LIKE("rainbow_like");
+
+    private final String code;
+
+    ItemTypeCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public static ItemTypeCode from(String code) {
+        try {
+            return ItemTypeCode.valueOf(code);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            throw new IllegalArgumentException("存在しないアイテムコードです");
+        }
+    }
+}
