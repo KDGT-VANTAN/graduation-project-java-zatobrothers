889cbe0028b9936739d48224015aae9000bde3d3
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-02-04 20:40:13 +0900

    build: 環境によってFirebaseプロジェクトを切り替える設定追加

--- reimi/frontend/reimi_app/lib/main.dart
+++ reimi/frontend/reimi_app/lib/main.dart
@@ -2,19 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:reimi_app/core/config/crashlytics_config.dart';
-import 'package:reimi_app/core/config/firebase_options_dev.dart' as dev;
 import 'package:reimi_app/core/i18n/strings.g.dart';
 import 'package:reimi_app/app/my_app.dart';
 import 'package:reimi_app/core/storage/shared_preferences_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:reimi_app/firebase_options/firebase_options_dev.dart' as dev;
+import 'package:reimi_app/firebase_options/firebase_options_prod.dart' as prod;
+
+// 環境変数から取得（dart-define-from-fileで渡される）
+const flavor = String.fromEnvironment('flavor');
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final firebaseOptions = flavor == 'prod'
+      ? prod.DefaultFirebaseOptions.currentPlatform
+      : dev.DefaultFirebaseOptions.currentPlatform;
+  await Firebase.initializeApp(options: firebaseOptions);
   final prefs = await SharedPreferences.getInstance();
   LocaleSettings.useDeviceLocale(); // デバイスのロケール設定に基づいて初期言語を設定
-  await Firebase.initializeApp(
-    options: dev.DefaultFirebaseOptions.currentPlatform,
-  );
   setupCrashlytics();
   runApp(
     ProviderScope(
