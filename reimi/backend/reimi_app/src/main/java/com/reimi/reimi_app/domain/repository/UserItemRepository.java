247da862cc7292db51341ca20ba5063049057556
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-02-07 23:19:33 +0900

    feat: ユーザーアイテムリポジトリのインターフェイス作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/repository/UserItemRepository.java
@@ -0,0 +1,7 @@
+package com.reimi.reimi_app.domain.repository;
+
+import com.reimi.reimi_app.domain.model.item.UserItem;
+
+public interface UserItemRepository {
+    void save(UserItem userItem);
+}
