deb0bd3218ecd4052d6c6d2c615e0d66a7f5a5ad
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-02-09 00:39:16 +0900

    feat: アウトプットDTOを作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/application/dto/output/UserItemOutput.java
@@ -0,0 +1,15 @@
+package com.reimi.reimi_app.application.dto.output;
+
+import java.util.Map;
+
+import com.reimi.reimi_app.domain.model.item.ItemTypeCode;
+import com.reimi.reimi_app.domain.model.user.UserId;
+import com.reimi.reimi_app.domain.model.weatherpersonality.UserWeatherPersonalityType;
+
+public record UserItemOutput(
+    UserId userId,
+    String name,
+    String mainPhotoUrl,
+    UserWeatherPersonalityType userWeatherPersonalityType,
+    Map<ItemTypeCode, Integer> items
+) {}
--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/application/usecase/ItemUseCase.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/application/usecase/ItemUseCase.java
@@ -1,6 +1,6 @@
 package com.reimi.reimi_app.application.usecase;
 
-import com.reimi.reimi_app.application.dto.UserItemOutput;
+import com.reimi.reimi_app.application.dto.output.UserItemOutput;
 
 public interface ItemUseCase {
     UserItemOutput getUserItemList();
