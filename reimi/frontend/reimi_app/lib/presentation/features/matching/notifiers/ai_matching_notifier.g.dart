be8edc403aa3ce83bdf46fe24246586ab0f3fad5
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-02-10 03:32:15 +0900

    feat: AIマッチング画面を状態管理するNotifier実装

---
+++ reimi/frontend/reimi_app/lib/presentation/features/matching/notifiers/ai_matching_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ai_matching_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$aIMatchingNotifierHash() =>
+    r'271b35d907e9ce5fafa810ff8147f397bab9ec08';
+
+/// See also [AIMatchingNotifier].
+@ProviderFor(AIMatchingNotifier)
+final aIMatchingNotifierProvider =
+    AutoDisposeNotifierProvider<AIMatchingNotifier, AIMatchingState>.internal(
+  AIMatchingNotifier.new,
+  name: r'aIMatchingNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$aIMatchingNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AIMatchingNotifier = AutoDisposeNotifier<AIMatchingState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
