6f01e479503a950c73b647afba99cc03baa86646
kiichi7580 <kiichi.nakazato0521@gmail.com>
2026-02-10 03:24:22 +0900

    res: LinearGradientパターンのレインボー色を追加

--- reimi/frontend/reimi_app/lib/core/theme/custom_colors.dart
+++ reimi/frontend/reimi_app/lib/core/theme/custom_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Gradient earthToneRainbowGradient = SweepGradient(
+const Gradient earthToneRainbowSweepGradient = SweepGradient(
   center: Alignment.center,
   startAngle: 0.0,
   endAngle: 6.283185307179586, // 2π
@@ -14,3 +14,19 @@ const Gradient earthToneRainbowGradient = SweepGradient(
     Color(0xFFB56576), // 戻し
   ],
 );
+
+const Gradient earthToneRainbowLinearGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [
+    Color(0xFFB56576),
+    Color(0xFFD4A373),
+    Color(0xFFE9C46A),
+    Color(0xFF7F9C8E),
+    Color(0xFF5F8D9C),
+    Color(0xFF6D597A),
+    Color(0xFFB56576),
+  ],
+);
+
+const weatherNewsColor = Color(0xFF2653A0);
