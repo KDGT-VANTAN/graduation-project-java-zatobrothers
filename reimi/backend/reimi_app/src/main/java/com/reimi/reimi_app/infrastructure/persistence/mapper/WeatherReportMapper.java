f3d6093e357c0c93485ade87bb3855ce5cd84487
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-02-12 21:29:34 +0900

    feat: WeatherReportMapperの作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/mapper/WeatherReportMapper.java
@@ -0,0 +1,49 @@
+package com.reimi.reimi_app.infrastructure.persistence.mapper;
+
+import com.reimi.reimi_app.domain.model.report.WeatherReport;
+import com.reimi.reimi_app.infrastructure.persistence.embeddable.WeatherObservationEmbeddable;
+import com.reimi.reimi_app.infrastructure.persistence.entity.WeatherMediaEntity;
+import com.reimi.reimi_app.infrastructure.persistence.entity.WeatherReportEntity;
+
+public class WeatherReportMapper {
+
+    public static WeatherReportEntity toEntity(WeatherReport weatherReport) {
+
+        WeatherReportEntity entity = new WeatherReportEntity();
+            entity.setId(weatherReport.getId().value());
+            entity.setUserId(weatherReport.getUserId().value());
+            entity.setComment(weatherReport.getComment());
+            entity.setWeatherType(weatherReport.getWeather());
+            entity.setFeelingType(weatherReport.getFeeling());
+            entity.setForecastType(weatherReport.getForecast());
+            entity.setLatitude(weatherReport.getLatitude());
+            entity.setLongitude(weatherReport.getLongitude());
+
+        weatherReport.getMediaList().forEach(media -> {
+            WeatherMediaEntity mediaEntity =
+                new WeatherMediaEntity(
+                    media.getId().value(),
+                    media.getMediaType(),
+                    media.getUrl(),
+                    entity
+                );
+
+            entity.getMediaList().add(mediaEntity);
+        });
+
+        if (weatherReport.getObservation() != null) {
+            WeatherObservationEmbeddable embeddable = new WeatherObservationEmbeddable();
+                new WeatherObservationEmbeddable(
+                    weatherReport.getObservation().getTemperature(),
+                    weatherReport.getObservation().getHumidity(),
+                    weatherReport.getObservation().getPressure(),
+                    weatherReport.getObservation().getWindSpeed(),
+                    weatherReport.getObservation().getWindDirection()
+                );
+
+            entity.setObservation(embeddable);
+        }
+
+        return entity;
+    }
+}
