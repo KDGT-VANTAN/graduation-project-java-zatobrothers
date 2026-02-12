c05f3b04894222038cc10ad0b29c72dbbf1a86ae
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-02-12 21:34:45 +0900

    feat: ウェザーリポートリポジトリの実装クラスを作成

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/persistence/repository/report/WeatherReportRepositoryImpl.java
@@ -0,0 +1,36 @@
+package com.reimi.reimi_app.infrastructure.persistence.repository.report;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Repository;
+
+import com.reimi.reimi_app.domain.model.report.WeatherReport;
+import com.reimi.reimi_app.domain.model.user.UserId;
+import com.reimi.reimi_app.domain.repository.WeatherReportRepository;
+import com.reimi.reimi_app.infrastructure.persistence.mapper.WeatherReportMapper;
+
+@Repository
+public class WeatherReportRepositoryImpl implements WeatherReportRepository {
+
+    private final JpaWeatherReportRepository jpaWeatherReportRepository;
+
+    public WeatherReportRepositoryImpl(
+        JpaWeatherReportRepository jpaWeatherReportRepository
+    ) {
+        this.jpaWeatherReportRepository = jpaWeatherReportRepository;
+    }
+
+    @Override
+    public boolean existsByUserIdAndDate(UserId userId, LocalDate date) {
+        return jpaWeatherReportRepository.existsByUserIdAndReportDate(
+            userId.value(),
+            date
+        );
+    }
+
+    @Override
+    public void save(WeatherReport weatherReport) {
+        jpaWeatherReportRepository.save(WeatherReportMapper.toEntity(weatherReport));
+    }
+
+}
