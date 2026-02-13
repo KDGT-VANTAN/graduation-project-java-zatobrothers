ed54b04ef3c9f18323bc516bf4b1d35a91f1683b
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-02-13 23:26:01 +0900

    feat: ウェザーリポートリポジトリインターフェイスにメソッドを追加

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/repository/WeatherReportRepository.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/domain/repository/WeatherReportRepository.java
@@ -1,6 +1,7 @@
 package com.reimi.reimi_app.domain.repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import com.reimi.reimi_app.domain.model.report.WeatherReport;
 import com.reimi.reimi_app.domain.model.user.UserId;
@@ -10,4 +11,6 @@ public interface WeatherReportRepository {
     boolean existsByUserIdAndDate(UserId userId, LocalDate date);
 
     void save(WeatherReport weatherReport);
+
+    List<WeatherReport> findByDate(LocalDate date);
 }
