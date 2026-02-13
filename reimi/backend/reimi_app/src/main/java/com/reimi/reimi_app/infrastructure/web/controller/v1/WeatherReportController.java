9ed5813abb4f8a2bc4f4f500d37cf61ef09b4aa0
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-02-13 23:54:39 +0900

    feat: ControllerにGET APIメソッド追加

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/v1/WeatherReportController.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/v1/WeatherReportController.java
@@ -1,8 +1,12 @@
 package com.reimi.reimi_app.infrastructure.web.controller.v1;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +16,8 @@ import com.reimi.reimi_app.application.command.report.WeatherMediaCommand;
 import com.reimi.reimi_app.application.command.report.WeatherObservationCommand;
 import com.reimi.reimi_app.application.usecase.WeatherReportUseCase;
 import com.reimi.reimi_app.infrastructure.web.dto.request.PostWeatherReportRequest;
+import com.reimi.reimi_app.infrastructure.web.dto.response.GetWeatherReportListResponse;
+import com.reimi.reimi_app.infrastructure.web.openapi.report.GetWeatherReportApi;
 import com.reimi.reimi_app.infrastructure.web.openapi.report.PostWeatherReportApi;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -67,5 +73,26 @@ public class WeatherReportController extends ApiV1Controller {
         );
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping(path = "/weather-reports")
+    @GetWeatherReportApi
+    public ResponseEntity<List<GetWeatherReportListResponse>> getTodayWeatherReports() {
+
+        LocalDate today = LocalDate.now();
+
+        List<GetWeatherReportListResponse> response = weatherReportUseCase.getTodayWeatherReportList(today)
+                .stream()
+                .map(report -> new GetWeatherReportListResponse(
+                    report.getId().value(),
+                    report.getUserId().value(),
+                    report.getComment(),
+                    report.getMedia().getMediaType(),
+                    report.getMedia().getSignedWeatherPhotoUrl(),
+                    report.getCreateAt()
+                ))
+                .toList();
+
+        return ResponseEntity.ok(response);
     }
 }
