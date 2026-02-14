77c630f8262717b1f88de313755b63a91b71c5dd
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-02-15 03:28:31 +0900

    feat: Controllerの作成＆ある地点での天気予報の詳細情報を取得できるGET APIメソッドを定義

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/v1/WeatherForecastController.java
@@ -0,0 +1,48 @@
+package com.reimi.reimi_app.infrastructure.web.controller.v1;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reimi.reimi_app.application.usecase.WeatherForecastUseCase;
+import com.reimi.reimi_app.infrastructure.web.dto.request.WeatherForecastRequest;
+import com.reimi.reimi_app.infrastructure.web.dto.response.GetWeatherForecastResponse;
+import com.reimi.reimi_app.infrastructure.web.openapi.weatherforecast.GetWeatherForecastApi;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+
+@RestController
+@Tag(name = "WeatherForecast", description = "天気予報関連のAPI")
+public class WeatherForecastController extends ApiV1Controller {
+
+    private final WeatherForecastUseCase weatherForecastUseCase;
+
+    public WeatherForecastController(
+        WeatherForecastUseCase weatherForecastUseCase
+    ) {
+        this.weatherForecastUseCase = weatherForecastUseCase;
+    }
+
+    @GetMapping(path = "/weather-forecasts")
+    @GetWeatherForecastApi
+    public ResponseEntity<GetWeatherForecastResponse> getWeatherForecast(
+        @Valid WeatherForecastRequest request
+    ) {
+        var result = weatherForecastUseCase
+            .getWeatherForecastDetail(
+                request.latitude(),
+                request.longitude()
+            );
+
+        GetWeatherForecastResponse response =
+            new GetWeatherForecastResponse(
+                result.latitude(),
+                result.longitude(),
+                result.srfs(),
+                result.mrfs()
+            );
+
+        return ResponseEntity.ok(response);
+    }
+}
