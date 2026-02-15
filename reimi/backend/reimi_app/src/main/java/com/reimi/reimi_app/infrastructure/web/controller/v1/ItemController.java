259d5e9d610be387e544c10e9fd065a4b9b3afeb
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-02-15 21:54:54 +0900

    feat: 継承させる

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/v1/ItemController.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/web/controller/v1/ItemController.java
@@ -15,7 +15,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @Tag(name = "User", description = "ユーザー関連のAPI")
-public class ItemController {
+public class ItemController extends ApiV1Controller {
 
     private final ItemUseCase itemUseCase;
 
