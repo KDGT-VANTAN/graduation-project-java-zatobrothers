5965fc33f8cb6c133fca7338e1dd580b62a785b2
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-02-16 20:55:32 +0900

    feat: アプリケーション起動ファイルを元に戻す

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/ReimiAppApplication.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/ReimiAppApplication.java
@@ -4,10 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import com.google.genai.Client;
-import com.google.genai.types.GenerateContentResponse;
-
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication(
 	exclude = UserDetailsServiceAutoConfiguration.class
@@ -17,25 +13,6 @@ public class ReimiAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReimiAppApplication.class, args);
-
-		Dotenv dotenv = Dotenv.load();
-
-        String apiKey = dotenv.get("GEMINI_API_KEY");
-        if (apiKey == null || apiKey.isEmpty()) {
-            throw new RuntimeException("GEMINI_API_KEY が設定されていません");
-        }
-
-        String model = dotenv.get("GEMINI_MODEL");
-
-		Client client = Client.builder().apiKey(apiKey).build();
-
-		GenerateContentResponse response =
-			client.models.generateContent(
-				model,
-				"Japan is an island nation known for its rich culture, advanced technology, and beautiful landscapes.",
-				null);
-
-		System.out.println(response.text());
 	}
 
 }
