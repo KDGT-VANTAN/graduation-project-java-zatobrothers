e9b6b82860d25dcef70ddca880b143c73d22f8b9
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-02-16 21:10:47 +0900

    feat: AIサジェスションユースケースのインターフェイスに業務を追加

---
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/application/usecase/AiSuggestionUseCase.java
@@ -0,0 +1,7 @@
+package com.reimi.reimi_app.application.usecase;
+
+import com.reimi.reimi_app.domain.external.ai.AiSuggestion;
+
+public interface AiSuggestionUseCase {
+    AiSuggestion generateSuggestion();
+}
