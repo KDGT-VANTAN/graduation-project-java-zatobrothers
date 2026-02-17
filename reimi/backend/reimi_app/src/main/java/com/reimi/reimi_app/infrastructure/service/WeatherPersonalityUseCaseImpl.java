6cc58c76d2275526d74d4f3d94b17ebb455ef227
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2026-02-17 21:29:07 +0900

    feat: ウェザーパーソナリティの実装ユースケースにオーバーライドメソッドを定義

--- reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/service/WeatherPersonalityUseCaseImpl.java
+++ reimi/backend/reimi_app/src/main/java/com/reimi/reimi_app/infrastructure/service/WeatherPersonalityUseCaseImpl.java
@@ -3,11 +3,13 @@ package com.reimi.reimi_app.infrastructure.service;
 import org.springframework.stereotype.Service;
 
 import com.reimi.reimi_app.application.command.DiagnoseWeatherPersonalityCommand;
+import com.reimi.reimi_app.application.exception.client.AccessDeniedException;
 import com.reimi.reimi_app.application.exception.client.DiagnoseResultAlreadyExistsException;
 import com.reimi.reimi_app.application.exception.client.ResourceNotFoundException;
 import com.reimi.reimi_app.application.service.ImageUrlResolver;
 import com.reimi.reimi_app.application.usecase.WeatherPersonalityUseCase;
 import com.reimi.reimi_app.domain.model.user.User;
+import com.reimi.reimi_app.domain.model.user.UserId;
 import com.reimi.reimi_app.domain.model.weatherpersonality.diagnose.WeatherPersonalityDiagnosis;
 import com.reimi.reimi_app.domain.model.weatherpersonality.diagnose.WeatherPersonalityScore;
 import com.reimi.reimi_app.domain.model.weatherpersonality.type.WeatherPersonalityCode;
@@ -111,5 +113,29 @@ public class WeatherPersonalityUseCaseImpl implements WeatherPersonalityUseCase 
         existing.update(type, weatherPersonalityScore);
 
         userWeatherPersonalityTypeRepository.save(existing);
+    }
+
+    @Override
+    public UserWeatherPersonalityType getUserResultDetail(UserId userId) {
+
+    String firebaseUid = authenticatedUserProvider.getFirebaseUid();
+
+    User loginUser = userRepository.findMeByFirebaseUid(firebaseUid)
+            .orElseThrow(() -> new ResourceNotFoundException("ユーザー"));
+
+    if (!loginUser.getId().equals(userId)) {
+        throw new AccessDeniedException();
+    }
+
+    UserWeatherPersonalityType result = userWeatherPersonalityTypeRepository.findByUserId(loginUser.getId())
+        .orElseThrow(() -> new ResourceNotFoundException("ウェザーパーソナリティ診断結果"));
+
+    String typeImageUrl = imageUrlResolver.resolve(
+            result.getWeatherPersonalityType().getImagePath()
+        );
+
+        result.getWeatherPersonalityType().setTypeImageUrl(typeImageUrl);
+
+        return result;
     }
 }
